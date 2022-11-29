a1c0a211328c64abc9a61f76315b4a964033a76b
Markus Rauhut <markusra@users.noreply.github.com>
2022-11-29 09:43:21 +0100

    Flere tilpasninger på søkesiden (#7)
    * Mindre refaktorering av søkesiden
    
    * Tilpasning av header og footer
    
    Co-authored-by: Markus Rauhut <markus.rauhut@vegvesen.no>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { useState } from 'react'
 import useSWRInfinite from 'swr/infinite'
-import { BodyShort, Select, Heading, Loader, Button } from '@navikt/ds-react'
+import { BodyShort, Select, Heading, Loader, Button, Alert } from '@navikt/ds-react'
 import { fetchProdukter, FetchResponse, PAGE_SIZE, SearchData } from '../utils/api-util'
+import { Produkt as ProduktType } from '../utils/produkt-util'
 
 import Produkt from './Produkt'
 import Sidebar from './Sidebar'
@@ -21,40 +22,52 @@ export default function Page() {
   const isLoading = !data || (size > 0 && data && typeof data[size - 1] === 'undefined')
   const isLastPage = data && data[data.length - 1]?.produkter.length < PAGE_SIZE
 
+  const Sokeresultater = ({ produkter }: { produkter: Array<ProduktType> | undefined }) => {
+    if (!produkter?.length) {
+      return (
+        <>
+          <Heading level="2" size="medium">
+            Søkeresultater
+          </Heading>
+          <Alert variant="info" fullWidth>
+            Ingen produkter funnet.
+          </Alert>
+        </>
+      )
+    }
+    return (
+      <>
+        <header className="results__header">
+          <div>
+            <Heading level="2" size="medium">
+              Søkeresultater
+            </Heading>
+            <BodyShort>{`${produkter.length} av ${data?.at(0)?.antallProdukter} produkter vises`}</BodyShort>
+          </div>
+          <Select label="Sortering" hideLabel={false} size="small" className="results__sort-select">
+            <option value="">Alfabetisk</option>
+          </Select>
+        </header>
+        <ol className="results__list">
+          {produkter.map((produkt) => (
+            <Produkt key={produkt.id} produkt={produkt} paaRammeavtale={false} />
+          ))}
+        </ol>
+        {!isLastPage ? (
+          <Button variant="secondary" onClick={() => setSize(size + 1)} loading={isLoading}>
+            Vis flere treff
+          </Button>
+        ) : null}
+      </>
+    )
+  }
+
   return (
     <div className="flex-wrapper">
       <Sidebar searchData={searchData} setSearchData={setSearchData} />
       <div className="results__wrapper">
         {!data && <Loader className="results__loader" size="3xlarge" title="Laster produkter" />}
-        {data && (
-          <>
-            <header className="results__header">
-              <div>
-                <Heading level="2" size="medium">
-                  Søkeresultater
-                </Heading>
-                <BodyShort>
-                  {produkter?.length
-                    ? `${produkter.length} av ${data?.at(0)?.antallProdukter} produkter vises`
-                    : 'Ingen produkter funnet'}
-                </BodyShort>
-              </div>
-              <Select label="Sortering" hideLabel={false} size="small" className="results__sort-select">
-                <option value="">Alfabetisk</option>
-              </Select>
-            </header>
-            <ol className="results__list">
-              {produkter?.map((produkt) => (
-                <Produkt key={produkt.id} produkt={produkt} paaRammeavtale={false} />
-              ))}
-            </ol>
-            {!isLastPage ? (
-              <Button variant="secondary" onClick={() => setSize(size + 1)} loading={isLoading}>
-                Vis flere treff
-              </Button>
-            ) : null}
-          </>
-        )}
+        {data && <Sokeresultater produkter={produkter} />}
       </div>
     </div>
   )
