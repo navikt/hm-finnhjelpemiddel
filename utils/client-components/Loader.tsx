f2504782a63d7269b6ecaee67c56db1124183a38
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2022-12-02 12:12:50 +0100

    rename folder

--- utils/client-components/Loader.tsx
+++ utils/client-components/Loader.tsx
@@ -1,12 +1,12 @@
 'use client'
 import { Loader } from '@navikt/ds-react'
 
-type LoaderUseClientProps = {
+type ClientLoaderProps = {
   size?: 'small' | '3xlarge' | '2xlarge' | 'xlarge' | 'large' | 'medium' | 'xsmall' | undefined
   title: string
 }
 
-const LoaderUseClient = ({ size, title }: LoaderUseClientProps) => {
+const ClientLoader = ({ size, title }: ClientLoaderProps) => {
   return <Loader className="results__loader" size={size} title={title}></Loader>
 }
-export default LoaderUseClient
+export default ClientLoader
