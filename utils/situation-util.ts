dd493bfa6f8ed769eaf20649c175017251a06266
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2022-12-13 15:55:09 +0100

    Start på veileder

---
+++ utils/situation-util.ts
@@ -0,0 +1,37 @@
+type Situations = Category[]
+
+export type Category = {
+  id: number
+  name: string
+  iconUrl: string
+}
+
+export const SituationCategoryName: Situations = [
+  { id: 1, name: 'Trenger tilrettelegging på jobb eller i utdanning', iconUrl: '/assets/icons/office.svg' },
+  {
+    id: 2,
+    name: 'Trenger tilrettelegging i barnehagen eller på skolen',
+    iconUrl: '/assets/icons/schoolbag.svg',
+  },
+  { id: 3, name: 'Trenger tilrettelegging av bolig eller fritid', iconUrl: '/assets/icons/home.svg' },
+  { id: 4, name: 'Har nedsatt hørsel', iconUrl: '/assets/icons/hearing.svg' },
+  { id: 5, name: 'Har nedsatt syn', iconUrl: '/assets/icons/sight.svg' },
+  { id: 6, name: 'Har vansker med tale og språk', iconUrl: '/assets/icons/dialog.svg' },
+  { id: 7, name: 'Har vansker med å huske, planlegge og organisere', iconUrl: '/assets/icons/calender.svg' },
+  { id: 8, name: 'Har vansker med bevegelse og forflytning', iconUrl: '/assets/icons/disability.svg' },
+  { id: 9, name: 'Har lese- og skrivevansker', iconUrl: '/assets/icons/write.svg' },
+  { id: 10, name: 'Trenge protese, ortose, ortopediske sko eller parykk', iconUrl: '' },
+]
+
+export const SituationCategoryIsoCodes = {
+  1: ['18'],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+  8: [],
+  9: [],
+  10: [],
+}
