d7abdfdbe3aaae02f016658543e8dfe030cac45d
Markus Rauhut <markus.rauhut@gmail.com>
2023-03-01 10:49:03 +0100

    Scroll restoration on page navigation and refresh
    - Add "to" as URL param (used for fetching the correct number of products and thus enabling scroll restoration)
    - Add useRestoreScroll hook for restoring scroll position between page navigation
    - Activate experimental scrollRestoration in Next.js (this prevents the page from jumping to the bottom before navigating)
    - Increase PAGE_SIZE from 15 to 25
    - Bump swr to v2.0.3
    - Remove "Til toppen" button in footer (is replaced by a floating button which is always visible)
    - Remove floating "Nullstill søket" button

---
+++ src/hooks/useRestoreScroll.ts
@@ -0,0 +1,58 @@
+import { useEffect, useLayoutEffect, useState } from 'react'
+import useDebounce from './useDebounce'
+
+export default function useRestoreScroll(id: string, shouldRestore: boolean) {
+  const [scrollTop, setScrollTop] = useState<Number>()
+  const debouncedScrollTop = useDebounce(scrollTop, 100)
+  const SESSION_STORAGE_ID = `restore-scroll-${id}`
+
+  /**
+   * Restore previous scroll position
+   */
+  useLayoutEffect(() => {
+    try {
+      if (shouldRestore) {
+        const storedValue = sessionStorage.getItem(SESSION_STORAGE_ID)
+        if (storedValue) {
+          window.scrollTo(0, parseInt(storedValue, 10))
+        }
+      }
+    } catch (error) {
+      // ignore sessionStorage error
+    }
+  }, [SESSION_STORAGE_ID, shouldRestore])
+
+  /**
+   * Listen to scroll event, and track current scroll position
+   */
+  useEffect(() => {
+    try {
+      if (shouldRestore) {
+        function handleScroll() {
+          setScrollTop(Math.round(window.pageYOffset || document.documentElement.scrollTop))
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+          window.removeEventListener('scroll', handleScroll)
+        }
+      }
+    } catch (error) {
+      // ignore sessionStorage error
+    }
+  }, [shouldRestore])
+
+  /**
+   * Store current scroll position
+   */
+  useEffect(() => {
+    try {
+      if (debouncedScrollTop !== undefined) {
+        sessionStorage.setItem(SESSION_STORAGE_ID, `${debouncedScrollTop}`)
+      }
+    } catch (error) {
+      // ignore sessionStorage error
+    }
+  }, [SESSION_STORAGE_ID, debouncedScrollTop])
+}
