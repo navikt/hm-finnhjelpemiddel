6a163992dd0ced12397bb0019043e81d07778a5e
arshadkamran <arshad.ali.kamran@nav.no>
2023-04-26 21:34:47 +0200

    ny forside, søk navigasjon, paths i tsconfig for import alias

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,132 +1,64 @@
-import { useEffect, useRef, useState } from 'react'
-import { InferGetServerSidePropsType, NextPageContext } from 'next'
-import Router from 'next/router'
-import useSWRInfinite from 'swr/infinite'
-import { initialSearchDataState, useHydratedSearchStore } from '../utils/search-state-util'
-import { CompareMode, useHydratedCompareStore } from '../utils/compare-state-util'
-import { fetchProducts, FetchResponse, PAGE_SIZE, SearchParams } from '../utils/api-util'
-import { mapProductSearchParams, toSearchQueryString } from '../utils/product-util'
+import React, {useState} from 'react';
+import {Button, Search, Switch} from "@navikt/ds-react";
+import {initialSearchDataState, useHydratedSearchStore} from "@/utils/search-state-util";
+import {Controller, FormProvider, SubmitHandler, useForm} from "react-hook-form";
+import {SearchData} from "@/utils/api-util";
+import {mapProductSearchParams} from "@/utils/product-util";
+import SelectIsoCategory from "@/components/sidebar/SelectIsoCategory";
+import FilterView from "@/components/sidebar/FilterView";
+import {Delete} from "@navikt/ds-icons";
+import {useRouter} from "next/router";
 
-import AnimateLayout from '../components/layout/AnimateLayout'
-import CompareMenu from '../components/compare-products/CompareMenu'
-import SearchResults from '../components/search/SearchResults'
-import Sidebar from '../components/sidebar/Sidebar'
-import { Button, Heading } from '@navikt/ds-react'
-import { useInView } from 'react-intersection-observer'
-import { Up } from '@navikt/ds-icons'
-
-export const getServerSideProps: (
-  context: NextPageContext
-) => Promise<{ props: { searchParams: SearchParams } }> = async (context: NextPageContext) => {
-  const { query } = context
-  return { props: { searchParams: mapProductSearchParams(query) } }
-}
-
-export default function Home({ searchParams }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const {
-    searchData,
-    setSearchData,
-    meta: { showProductSeriesView },
-  } = useHydratedSearchStore()
-  const { compareMode } = useHydratedCompareStore()
-
-  const [initialProductSearchParams, setInitialProductSearchParams] = useState(searchParams)
-  const [searchInitialized, setSearchInitialized] = useState(false)
-
-  const { data, size, setSize, isLoading } = useSWRInfinite<FetchResponse>(
-    (index) => {
-      const from = index !== 0 ? (initialProductSearchParams.to || PAGE_SIZE) + (index - 1) * PAGE_SIZE : 0
-      const to = (index === 0 && initialProductSearchParams.to) || PAGE_SIZE
-      return {
-        url: `/products/_search`,
-        from,
-        to,
-        searchData,
-        isProductSeriesView: showProductSeriesView,
-      }
-    },
-    fetchProducts,
-    {
-      keepPreviousData: true,
-    }
-  )
-
-  const { ref: pageTopRef, inView: isAtPageTop } = useInView({ threshold: 0.4 })
-  const productViewToggleRef = useRef<HTMLButtonElement>(null)
-
-  const setFocusOnSearchResults = () => {
-    productViewToggleRef.current && productViewToggleRef.current.focus()
+function Home(props: any) {
+  const router = useRouter()
+  const FocusOnResultsButton = ({ setFocus }: { setFocus: () => void }) => {
+    return (
+      <div className="search__focus-on-results">
+        <Button variant="secondary" size="small" type="button" onClick={setFocus}>
+          Gå til resultat
+        </Button>
+      </div>
+    )
   }
-
-  useEffect(() => setSearchData(initialProductSearchParams), [initialProductSearchParams, setSearchData])
-
-  useEffect(() => {
-    if (data) {
-      setSearchInitialized(true)
-    }
-  }, [data])
-
-  const products = data?.flatMap((d) => d.products)
-  const numberOfProducts = products && products.length > PAGE_SIZE ? products.length : undefined
-
-  useEffect(() => {
-    if (searchInitialized) {
-      Router.push(
-        toSearchQueryString({
-          ...searchData,
-          to: numberOfProducts,
-        }),
-        undefined,
-        {
-          scroll: false,
-        }
-      )
-    }
-  }, [searchInitialized, searchData, numberOfProducts])
+  const onSubmit: SubmitHandler<SearchData> = (data) => {
+    router.push('/sok?term='+ data.searchTerm)
+  }
+  const [productSearchParams] = useState(mapProductSearchParams(router.query))
+  const formMethods = useForm<SearchData>({
+    defaultValues: {
+      ...initialSearchDataState,
+      ...productSearchParams,
+    },
+  })
+  const { control, handleSubmit, reset: resetForm, setValue } = formMethods
+  const { searchData, setSearchData } = useHydratedSearchStore()
 
   return (
-    <>
-      {compareMode === CompareMode.Active && <CompareMenu />}
-      <AnimateLayout>
-        <div className="main-header">
-          <Heading level="1" size="large" ref={pageTopRef}>
-            Hjelpemiddeloppslag
-          </Heading>
-        </div>
-        <div className="main-wrapper">
-          <div className="flex-column-wrap">
-            <Sidebar
-              filters={data?.at(-1)?.filters}
-              onResetSearchData={() => {
-                setInitialProductSearchParams({ ...initialSearchDataState, to: undefined })
-                setSize(1)
-              }}
-              setFocus={setFocusOnSearchResults}
-            />
-            <section className="results__wrapper">
-              {
-                <SearchResults
-                  data={data}
-                  size={size}
-                  setSize={setSize}
-                  isLoading={isLoading}
-                  productViewToggleRef={productViewToggleRef}
+    <div className="searchbar">
+      <div className="searchbar__heading">
+            Alt* av hjelpemidler samlet på en plass
+      </div>
+      <div className="searchbar__input">
+        <FormProvider {...formMethods}>
+          <form role="search" onSubmit={handleSubmit(onSubmit)} aria-controls="searchResults">
+            <Controller
+              render={({ field }) => (
+                <Search
+                  className="search__input"
+                  label="Skriv ett eller flere søkeord"
+                  hideLabel={false}
+                  {...field}
                 />
-              }
-            </section>
-          </div>
-        </div>
-        {!isAtPageTop && (
-          <Button
-            type="button"
-            className="search__page-up-button"
-            icon={<Up title="Gå til toppen av siden" />}
-            onClick={() => setFocusOnSearchResults()}
-          >
-            Til toppen
-          </Button>
-        )}
-      </AnimateLayout>
-    </>
-  )
+              )}
+              name="searchTerm"
+              control={control}
+              defaultValue=""
+            />
+          </form>
+        </FormProvider>
+      </div>
+    </div>
+  );
 }
+
+export default Home;
