a371f57f1f206a40a137f17f2ef986d3e57ee957
Markus Rauhut <markus.rauhut@gmail.com>
2023-04-26 10:47:22 +0200

    Adjust filter view
    - Show placeholder text if there are no available filters
    - Add a button to toggle between showing the first 10 filter values and all values

--- src/components/sidebar/internals/CheckboxFilterInput.tsx
+++ src/components/sidebar/internals/CheckboxFilterInput.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
-import { Checkbox, CheckboxGroup } from '@navikt/ds-react'
+import { Button, Checkbox, CheckboxGroup } from '@navikt/ds-react'
 import { FilterCategories } from '../../../utils/filter-util'
 import { Filter, SearchData } from '../../../utils/api-util'
 import { useHydratedSearchStore } from '../../../utils/search-state-util'
@@ -11,6 +11,7 @@ type CheckboxFilterInputProps = {
 }
 export const CheckboxFilterInput = ({ filter }: CheckboxFilterInputProps) => {
   const { key: filterKey, data: filterData } = filter
+  const [showAllValues, setShowAllValues] = useState(false)
 
   const { searchData, setFilter } = useHydratedSearchStore()
   const {
@@ -36,6 +37,8 @@ export const CheckboxFilterInput = ({ filter }: CheckboxFilterInputProps) => {
     (f) => !(filterData?.values.map((f) => f.key) || []).includes(f)
   )
 
+  const selectedInvisibleFilters = filterData?.values.slice(10).filter((f) => watchFilter.includes(f.key))
+
   if (!searchDataFilters.includes(filterKey) && !hasFilterData) {
     return null
   }
@@ -44,24 +47,43 @@ export const CheckboxFilterInput = ({ filter }: CheckboxFilterInputProps) => {
     <details open={watchFilter.length > 0 || touched}>
       <summary>{FilterCategories[filterKey]}</summary>
       <div>
-        <Controller
-          control={control}
-          name={`filters.${filterKey}`}
-          render={({ field }) => (
-            <CheckboxGroup legend={FilterCategories[filterKey]} hideLegend {...field} size="small">
-              {selectedUnavailableFilters?.map((f) => (
-                <Checkbox value={f} key={f}>
-                  {capitalize(String(f))} (0)
-                </Checkbox>
-              ))}
-              {filterData?.values.map((f) => (
-                <Checkbox value={f.key} key={f.key}>
-                  {capitalize(String(f.key))} ({f.doc_count})
-                </Checkbox>
-              ))}
-            </CheckboxGroup>
-          )}
-        />
+        <div className="checkbox-filter-input">
+          <Controller
+            control={control}
+            name={`filters.${filterKey}`}
+            render={({ field }) => (
+              <CheckboxGroup legend={FilterCategories[filterKey]} hideLegend {...field} size="small">
+                {selectedUnavailableFilters?.map((f) => (
+                  <Checkbox value={f} key={f}>
+                    {capitalize(String(f))} (0)
+                  </Checkbox>
+                ))}
+                {filterData?.values.slice(0, 10).map((f) => (
+                  <Checkbox value={f.key} key={f.key}>
+                    {capitalize(String(f.key))} ({f.doc_count})
+                  </Checkbox>
+                ))}
+                {showAllValues &&
+                  filterData?.values.slice(10).map((f) => (
+                    <Checkbox value={f.key} key={f.key}>
+                      {capitalize(String(f.key))} ({f.doc_count})
+                    </Checkbox>
+                  ))}
+                {!showAllValues &&
+                  selectedInvisibleFilters?.map((f) => (
+                    <Checkbox value={f.key} key={f.key}>
+                      {capitalize(String(f.key))} ({f.doc_count})
+                    </Checkbox>
+                  ))}
+              </CheckboxGroup>
+            )}
+          />
+        </div>
+        {filterData?.values && filterData?.values.length > 10 && (
+          <Button type="button" variant="tertiary" size="small" onClick={() => setShowAllValues((v) => !v)}>
+            {!showAllValues ? 'Vis alle' : 'Vis kun utvalgte'}
+          </Button>
+        )}
       </div>
     </details>
   )
