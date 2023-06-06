a557a7c102e07da36698010e973a88dcb10e5a85
Vilde Roksand Fauchald <vilde.fauchald@bekk.no>
2023-06-06 15:53:32 +0200

    Change from BodyShort to BodyLong in Footer due to line height

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import NextLink from 'next/link'
-import { BodyShort } from '@navikt/ds-react'
+import { BodyLong, BodyShort } from '@navikt/ds-react'
 
 const Footer = () => (
   <footer className="nav-bunn">
@@ -9,10 +9,10 @@ const Footer = () => (
       <div className="nav-bunn__info">
         <Image src="/nav-logo-black.svg" alt="Test" width={60} height={37} />
         <div>
-          <BodyShort>
+          <BodyLong>
             <b>Finn hjelpemidler</b>
-          </BodyShort>
-          <BodyShort>Finn hjelpemidler er en tjeneste fra NAV</BodyShort>
+          </BodyLong>
+          <BodyLong>Finn hjelpemidler er en tjeneste fra NAV</BodyLong>
           <NextLink href="/om-nettstedet">Om nettstedet</NextLink>
         </div>
       </div>
