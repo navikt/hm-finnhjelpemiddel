f18f31b753e1db5c2893005ceeeee71fd51b9c7a
Vilde Roksand Fauchald <vilde.fauchald@bekk.no>
2023-08-01 10:19:32 +0200

    Keep section even though productsOnPost is 0 and the rank is 1

--- src/app/produkt/[id]/AgreementInfo.tsx
+++ src/app/produkt/[id]/AgreementInfo.tsx
@@ -7,7 +7,7 @@ import { smallImageLoader } from '@/utils/image-util'
 import { Product } from '@/utils/product-util'
 
 import AgreementIcon from '@/components/AgreementIcon'
-import { Alert, Heading, ImageIcon } from '@/components/aksel-client'
+import { Alert, BodyLong, BodyShort, Heading, ImageIcon } from '@/components/aksel-client'
 
 type AgreementInfoProps = {
   product: Product
@@ -15,9 +15,6 @@ type AgreementInfoProps = {
 }
 
 export const AgreementInfo = ({ product, productsOnPost }: AgreementInfoProps) => {
-  if (productsOnPost?.length === 0 && product.applicableAgreementInfo?.rank === 1) {
-    return null
-  }
   return (
     <section className="agreement-details" aria-label="Informasjon om rammeavtalen produktet er på">
       <div className="agreement-details__content max-width">
@@ -31,13 +28,14 @@ export const AgreementInfo = ({ product, productsOnPost }: AgreementInfoProps) =
             høyere rangerte produkter for å se om det passer ditt behov.
           </Alert>
         )}
-        {productsOnPost && (
-          <div className="agreement-details__products-on-post">
-            <Heading level="4" size="medium" spacing>
-              Andre produkter på samme delkontrakt
-            </Heading>
+
+        <div className="agreement-details__products-on-post">
+          <Heading level="4" size="medium" spacing>
+            Andre produkter på samme delkontrakt
+          </Heading>
+          {productsOnPost && productsOnPost.length > 0 ? (
             <div className="agreement-details__products-on-post-list">
-              {productsOnPost.map((product) => (
+              {productsOnPost?.map((product) => (
                 <div className="agreement-details__product-on-post" key={product.id}>
                   <div className="image-container">
                     <div className="image">
@@ -71,8 +69,10 @@ export const AgreementInfo = ({ product, productsOnPost }: AgreementInfoProps) =
                 </div>
               ))}
             </div>
-          </div>
-        )}
+          ) : (
+            <BodyLong>Det finnes ingen andre produkter på samme delkontrakt</BodyLong>
+          )}
+        </div>
       </div>
     </section>
   )
