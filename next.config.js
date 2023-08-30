d77baa2ed2495bded8180ff9a9ee088c2d17536b
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2023-08-30 12:31:31 +0200

    Bugfix: redirect til gamle fila dersom det er en gammel lenke i beskrivelsen til rammeavtalen

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
         source: '/products/_search:path*',
         destination: process.env.HM_SEARCH_URL + '/products/_search:path*',
       },
+      { source: '/rammeavtale/blobs/:path*', destination: 'https://www.hjelpemiddeldatabasen.no/blobs/:path*' },
     ]
   },
   images: {
