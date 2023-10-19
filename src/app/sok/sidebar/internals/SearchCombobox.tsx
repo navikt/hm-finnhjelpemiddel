6b804ae5aab256d33b111bd33b63a71e2980754a
Vilde Roksand Fauchald <vilde.fauchald@bekk.no>
2023-10-19 15:27:59 +0200

    set list closed when not writing

--- src/app/sok/sidebar/internals/SearchCombobox.tsx
+++ src/app/sok/sidebar/internals/SearchCombobox.tsx
@@ -18,7 +18,8 @@ const SearchCombobox = ({ initialValue, onSearch }: Props) => {
   const [inputValue, setInputValue] = useState(initialValue)
   const [selectedOptions, setSelectedOptions] = useState<string[]>([initialValue])
   const { searchData } = useHydratedSearchStore()
-  const debouncedSearchValue = useDebounce<string>(inputValue, 200)
+  const debouncedSearchValue = useDebounce<string>(inputValue)
+  const [isListOpen, setListIsopen] = useState(false)
 
   const { data: suggestionData } = useSWR<SuggestionsResponse>(debouncedSearchValue, fetchSuggestions, {
     keepPreviousData: false,
@@ -49,37 +50,46 @@ const SearchCombobox = ({ initialValue, onSearch }: Props) => {
     }
   }, [searchData.searchTerm])
 
+  useEffect(() => {
+    if (inputValue) setListIsopen(true)
+    else setListIsopen(false)
+  }, [inputValue])
+
   const onToggleSelected = (option: string, isSelected: boolean) => {
     if (isSelected) {
       setSelectedOptions([option])
+      setListIsopen(false)
       onSearch(option)
     } else {
       setSelectedOptions([])
       onSearch('')
     }
   }
-  const options = searchData.hasAgreementsOnly
-    ? suggestionsWithAgreementOnly.length
-      ? suggestionsWithAgreementOnly
-      : ['Ingen søkeforslag']
-    : allSuggestionsSortedonAgreementValue.length
-    ? allSuggestionsSortedonAgreementValue
-    : ['Ingen søkeforslag']
+
+  const options = searchData.hasAgreementsOnly ? suggestionsWithAgreementOnly : allSuggestionsSortedonAgreementValue
 
   return (
     <UNSAFE_Combobox
       label="Skriv ett eller flere søkeord"
       isMultiSelect={false}
-      onChange={(event) => setInputValue(event?.target.value || '')}
+      // allowNewValues
+      onChange={(event) => {
+        setInputValue(event?.target.value || '')
+      }}
       onToggleSelected={onToggleSelected}
       selectedOptions={selectedOptions}
       options={options}
       value={inputValue}
-      toggleListButton={inputValue ? true : false}
+      clearButton={true}
+      isListOpen={isListOpen}
+      toggleListButton={false}
       onKeyUpCapture={(event) => {
         if (event.key === 'Enter') {
           event.preventDefault()
+          setSelectedOptions([inputValue])
           onSearch(inputValue)
+          setInputValue('')
+          setListIsopen(false)
         }
       }}
     />
