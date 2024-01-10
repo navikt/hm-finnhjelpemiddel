cdb818df8d6a6479a9ef79e1fd0d7ff0f1356ae2
Vilde Roksand Fauchald <vilde.fauchald@bekk.no>
2024-01-10 11:02:50 +0100

    fix issues with seriesId being favicon.

--- src/app/sammenlign/page.tsx
+++ src/app/sammenlign/page.tsx
@@ -4,7 +4,6 @@ import { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Sammenligner',
   description: 'Sammenlign produkter',
-  icons: [{ rel: 'icon', type: 'image/x-icon', url: 'favicon.ico', sizes: 'any' }],
 }
 
 export default function Page() {
