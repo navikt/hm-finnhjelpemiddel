6bc4cd9f743c95e95a3a021c922b48c6c526817c
Vilde Roksand Fauchald <vilde.fauchald@bekk.no>
2024-01-15 12:05:06 +0100

    add mail to banner

--- src/app/layoutProvider.tsx
+++ src/app/layoutProvider.tsx
@@ -108,7 +108,8 @@ function LayoutProvider({ children }: { children: React.ReactNode }) {
             <BodyLong>
               <b>Hei!</b> Denne siden er under kontinuerlig utvikling og vil på sikt erstatte Hjelpemiddeldatabasen.
               Foreløpig er ikke alt innhold og alle funksjoner på plass på denne siden. Dersom du ikke finner det du
-              leter etter anbefaler vi å bruke {''}
+              leter, gi oss gjerne beskjed på <Link href="mailto:finnhjelpemiddel@nav.no">finnhjelpemiddel@nav.no</Link>{' '}
+              eller bruk {''}
               <Link href="https://www.hjelpemiddeldatabasen.no/"> hjelpemiddeldatabasen.no</Link>
             </BodyLong>
           </div>
