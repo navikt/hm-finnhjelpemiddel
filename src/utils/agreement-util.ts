66fa7cd3544a188cae3772eee61bd10e0a2570f4
Vilde Roksand Fauchald <vilde.fauchald@bekk.no>
2024-01-31 10:36:23 +0100

    test in dev

--- src/utils/agreement-util.ts
+++ src/utils/agreement-util.ts
@@ -67,17 +67,29 @@ export const mapAgreement = (source: AgreementsSourceResponse): Agreement => {
   }
 }
 
+const excludedAgreementsDev: Record<string, string> = {
+  Bilombygg: '123ea1cc-f366-4e08-a40c-8f50eafcdc78',
+  Biler: '970d867a-d095-42a8-9f0a-495e42f301cb',
+  Servicehunder: '744ca191-ed99-4a09-90f0-29f3733885f5',
+  Høreapparater: '67196a0e-d1db-4c74-87ce-9d84da279c0a',
+}
+
+const excludedAgreementsProd: Record<string, string> = {
+  Bilombygg: 'c7a188d1-c54c-41de-9869-f40021738d37',
+  Biler: 'f082242e-e3c1-4f73-bbf6-9e06f3be9721',
+  Servicehunder: '8d55f208-9a78-4952-a4d1-cbb706bd7d9c',
+  Høreapparater: 'd73b510b-0043-4c9e-92ac-25b4ace236c9',
+}
+
 export const mapAgreementLabels = (data: SearchResponse): AgreementLabel[] => {
   //TODO: Når vi skrur av hjelpemiddelbasen kan disse slettes fra database og vi trenger ikke lenger å filterre de ut her.
   //Enn så lenge må vi gjøre det fordi de vil synkes fra hjelpemiddeldatabasen daglig
-  const excludedAgreements: Record<string, string> = {
-    Bilombygg: 'c7a188d1-c54c-41de-9869-f40021738d37',
-    Biler: 'f082242e-e3c1-4f73-bbf6-9e06f3be9721',
-    Servicehunder: '8d55f208-9a78-4952-a4d1-cbb706bd7d9c',
-    Høreapparater: 'd73b510b-0043-4c9e-92ac-25b4ace236c9',
-  }
 
-  const excludedValues = Object.values(excludedAgreements)
+  const env = process.env.NODE_ENV
+
+  console.log('\n\nENV', env)
+
+  const excludedValues = Object.values(env === 'production' ? excludedAgreementsProd : excludedAgreementsDev)
 
   return data.hits.hits
     .filter((hit) => !excludedValues.includes(hit._source.id))
