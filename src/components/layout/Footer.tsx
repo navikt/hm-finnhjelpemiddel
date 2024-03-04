438f7a472c919bab8a2de6bdf587d88ab55e396a
Arshad Ali Kamran <arshad.ali.kamran@nav.no>
2024-03-04 13:24:14 +0100

    first round of static pages without lists

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image'
 import NextLink from 'next/link'
 
-import { BodyShort, Link } from '@navikt/ds-react'
+import { Link } from '@navikt/ds-react'
 
 const Footer = () => (
   <footer className="nav-bunn">
@@ -11,10 +11,8 @@ const Footer = () => (
       <div className="nav-bunn__info">
         <Image src="/nav-logo-white.svg" alt="NAV-logo" width={64} height={20} />
         <div>
-          <BodyShort>
-            <b>FinnHjelpemiddel</b>
-          </BodyShort>
-          <BodyShort>FinnHjelpemiddel er en tjeneste fra NAV</BodyShort>
+          <NextLink href="/til-leverandorer">Til leverandører</NextLink>
+          <NextLink href="/rettigheter-og-ansvar">Rettigheter og ansvar</NextLink>
           <NextLink href="/om-nettstedet">Om FinnHjelpemiddel</NextLink>
           <Link
             href="https://uustatus.no/nb/erklaringer/publisert/d760f748-feec-4cdb-b1cb-9535f1219060"
