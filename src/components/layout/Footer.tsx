f1cd9e35fb5f30b444178e6432bad44fcb49813e
Arshad Ali Kamran <arshad.ali.kamran@nav.no>
2024-03-04 21:53:17 +0100

    Endre alle lenker i bunntekst til Aksel Link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import Image from 'next/image'
-import NextLink from 'next/link'
 
 import { Link } from '@navikt/ds-react'
 
@@ -11,9 +10,9 @@ const Footer = () => (
       <div className="nav-bunn__info">
         <Image src="/nav-logo-white.svg" alt="NAV-logo" width={64} height={20} />
         <div>
-          <NextLink href="/om-nettstedet">Om FinnHjelpemiddel</NextLink>
-          <NextLink href="/rettigheter-og-ansvar">Rettigheter og ansvar</NextLink>
-          <NextLink href="/til-leverandorer">Til leverandører</NextLink>
+          <Link href="/om-nettstedet">Om FinnHjelpemiddel</Link>
+          <Link href="/rettigheter-og-ansvar">Rettigheter og ansvar</Link>
+          <Link href="/til-leverandorer">Til leverandører</Link>
           <Link
             href="https://uustatus.no/nb/erklaringer/publisert/d760f748-feec-4cdb-b1cb-9535f1219060"
             rel="noopener noreferrer"
