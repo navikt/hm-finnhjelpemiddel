fb345caa7b3887e7be53c3712924dbfe058f9cc6
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2024-03-13 14:15:32 +0100

    Ta bort active-filter på sammenligneren

--- src/utils/api-util.ts
+++ src/utils/api-util.ts
@@ -1061,13 +1061,6 @@ export const fetchProductsWithVariants = (seriesIds: string[]): Promise<FetchSer
               seriesId: seriesIds,
             },
           },
-          filter: [
-            {
-              term: {
-                status: 'ACTIVE',
-              },
-            },
-          ],
         },
       },
       sort: [{ _score: { order: 'desc' } }, { 'agreements.postNr': 'asc' }, { 'agreements.rank': 'asc' }],
