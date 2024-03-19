cacb111968791e7d2814a9ecb164757f950f1e39
Vilde Roksand Fauchald <vilde.fauchald@bekk.no>
2024-03-19 20:38:49 +0100

    flytter ref og frestyler meny

--- src/app/BurgerMenuContent.tsx
+++ src/app/BurgerMenuContent.tsx
@@ -3,13 +3,12 @@ import { logNavigationEvent } from '@/utils/amplitude'
 import { getAgreementLabels } from '@/utils/api-util'
 import { sortAlphabetically } from '@/utils/sort-util'
 import { ChevronRightIcon } from '@navikt/aksel-icons'
-import { Heading, Link } from '@navikt/ds-react'
+import { HStack, Heading, Link } from '@navikt/ds-react'
 import NextLink from 'next/link'
 import { useRouter } from 'next/navigation'
-import { useCallback, useMemo, useRef } from 'react'
+import { useCallback, useMemo } from 'react'
 import useSWR from 'swr'
 import AutocompleteSearch from '../components/filters/AutocompleteSearch'
-import useOnClickOutside from '@/hooks/useOnClickOutside'
 
 interface Props {
   searchOpen: boolean
@@ -22,11 +21,6 @@ interface Props {
 const BurgerMenuContent = ({ searchOpen, menuOpen, setMenuOpen, setSearchOpen }: Props) => {
   const { data: agreements } = useSWR<AgreementLabel[]>('/agreements/_search', getAgreementLabels, {
     keepPreviousData: true,
-  })
-  const outerContainerRef = useRef<HTMLDivElement>(null)
-
-  useOnClickOutside(outerContainerRef, () => {
-    setMenuOpen(false)
   })
 
   const router = useRouter()
@@ -51,46 +45,64 @@ const BurgerMenuContent = ({ searchOpen, menuOpen, setMenuOpen, setSearchOpen }:
   return (
     <>
       {menuOpen && (
-        <div className="burgermenu-container" ref={outerContainerRef}>
+        <div className="burgermenu-container">
           <div className="burgermenu-container__content spacing-vertical--medium  main-wrapper--xlarge">
             <>
-              <Heading level="2" size="small">
-                Avtale med NAV
-              </Heading>
-              <ul>
-                <li>
-                  <Link
-                    className="burgermenu-container__link"
-                    as={NextLink}
-                    href="/rammeavtale"
-                    onClick={() => {
-                      setMenuOpen(false)
-                      logNavigationEvent('meny', 'rammeavtale', 'Avtaler med NAV')
-                    }}
-                  >
-                    <ChevronRightIcon aria-hidden title="Pil mot høyre" fontSize="1.5rem" />
-                    Avtaler med NAV
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="burgermenu-container__link"
-                    as={NextLink}
-                    href="/rammeavtale#se-at-et-hjelpemiddel-er-på-avtale"
-                    onClick={() => {
-                      setMenuOpen(false)
-                      logNavigationEvent(
-                        'meny',
-                        'rammeavtale',
-                        'Slik kan du se at et hjelpemiddel er på avtale med NAV'
-                      )
-                    }}
-                  >
-                    <ChevronRightIcon title="Pil mot høyre" fontSize="1.5rem" />
-                    Slik kan du se at et hjelpemiddel er på avtale med NAV
-                  </Link>
-                </li>
-              </ul>
+              <HStack>
+                <ul>
+                  <Heading level="2" size="small">
+                    Avtale med NAV
+                  </Heading>
+                  <li>
+                    <Link
+                      className="burgermenu-container__link"
+                      as={NextLink}
+                      href="/rammeavtale"
+                      onClick={() => {
+                        setMenuOpen(false)
+                        logNavigationEvent('meny', 'rammeavtale', 'Avtaler med NAV')
+                      }}
+                    >
+                      <ChevronRightIcon aria-hidden title="Pil mot høyre" fontSize="1.5rem" />
+                      Avtaler med NAV
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="burgermenu-container__link"
+                      as={NextLink}
+                      href="/rammeavtale#se-at-et-hjelpemiddel-er-på-avtale"
+                      onClick={() => {
+                        setMenuOpen(false)
+                        logNavigationEvent(
+                          'meny',
+                          'rammeavtale',
+                          'Slik kan du se at et hjelpemiddel er på avtale med NAV'
+                        )
+                      }}
+                    >
+                      <ChevronRightIcon title="Pil mot høyre" fontSize="1.5rem" />
+                      Slik kan du se at et hjelpemiddel er på avtale med NAV
+                    </Link>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <Heading level="2" size="small" style={{ marginTop: '4px' }}>
+                      Leverandører
+                    </Heading>
+                    <Link
+                      className="burgermenu-container__link"
+                      as={NextLink}
+                      href="/leverandorer"
+                      onClick={() => setMenuOpen(false)}
+                    >
+                      <ChevronRightIcon aria-hidden title="Pil mot høyre" fontSize="1.5rem" />
+                      Leverandøroversikt
+                    </Link>
+                  </li>
+                </ul>
+              </HStack>
               <Heading level="2" size="small" style={{ marginTop: '4px' }}>
                 Hjelpemidler på avtale med NAV
               </Heading>
@@ -112,18 +124,6 @@ const BurgerMenuContent = ({ searchOpen, menuOpen, setMenuOpen, setSearchOpen }:
                   </li>
                 ))}
               </ul>
-              <Heading level="2" size="small" style={{ marginTop: '4px' }}>
-                Leverandører
-              </Heading>
-              <Link
-                className="burgermenu-container__link"
-                as={NextLink}
-                href="/leverandorer"
-                onClick={() => setMenuOpen(false)}
-              >
-                <ChevronRightIcon aria-hidden title="Pil mot høyre" fontSize="1.5rem" />
-                Leverandøroversikt
-              </Link>
             </>
           </div>
         </div>
