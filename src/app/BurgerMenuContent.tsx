40378d443de74fb12a504b033abf5f267aa75053
Vilde Roksand Fauchald <vilde.fauchald@bekk.no>
2024-03-20 12:22:06 +0100

    fix layout on links

--- src/app/BurgerMenuContent.tsx
+++ src/app/BurgerMenuContent.tsx
@@ -49,64 +49,68 @@ const BurgerMenuContent = ({ searchOpen, menuOpen, setMenuOpen, setSearchOpen }:
           <div className="burgermenu-container__content spacing-vertical--medium  main-wrapper--xlarge">
             <>
               <HStack>
-                <ul>
+                <div>
                   <Heading level="2" size="small">
                     Avtale med NAV
                   </Heading>
-                  <li>
-                    <Link
-                      className="burgermenu-container__link"
-                      as={NextLink}
-                      href="/rammeavtale"
-                      onClick={() => {
-                        setMenuOpen(false)
-                        logNavigationEvent('meny', 'rammeavtale', 'Avtaler med NAV')
-                      }}
-                    >
-                      <ChevronRightIcon aria-hidden title="Pil mot høyre" fontSize="1.5rem" />
-                      Avtaler med NAV
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="burgermenu-container__link"
-                      as={NextLink}
-                      href="/rammeavtale#se-at-et-hjelpemiddel-er-på-avtale"
-                      onClick={() => {
-                        setMenuOpen(false)
-                        logNavigationEvent(
-                          'meny',
-                          'rammeavtale',
-                          'Slik kan du se at et hjelpemiddel er på avtale med NAV'
-                        )
-                      }}
-                    >
-                      <ChevronRightIcon title="Pil mot høyre" fontSize="1.5rem" />
-                      Slik kan du se at et hjelpemiddel er på avtale med NAV
-                    </Link>
-                  </li>
-                </ul>
-                <ul>
-                  <li>
-                    <Heading level="2" size="small" style={{ marginTop: '4px' }}>
-                      Leverandører
-                    </Heading>
-                    <Link
-                      className="burgermenu-container__link"
-                      as={NextLink}
-                      href="/leverandorer"
-                      onClick={() => setMenuOpen(false)}
-                    >
-                      <ChevronRightIcon aria-hidden title="Pil mot høyre" fontSize="1.5rem" />
-                      Leverandøroversikt
-                    </Link>
-                  </li>
-                </ul>
+                  <ul>
+                    <li>
+                      <Link
+                        className="burgermenu-container__link"
+                        as={NextLink}
+                        href="/rammeavtale"
+                        onClick={() => {
+                          setMenuOpen(false)
+                          logNavigationEvent('meny', 'rammeavtale', 'Avtaler med NAV')
+                        }}
+                      >
+                        <ChevronRightIcon aria-hidden title="Pil mot høyre" fontSize="1.5rem" />
+                        Avtaler med NAV
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="burgermenu-container__link"
+                        as={NextLink}
+                        href="/rammeavtale#se-at-et-hjelpemiddel-er-på-avtale"
+                        onClick={() => {
+                          setMenuOpen(false)
+                          logNavigationEvent(
+                            'meny',
+                            'rammeavtale',
+                            'Slik kan du se at et hjelpemiddel er på avtale med NAV'
+                          )
+                        }}
+                      >
+                        <ChevronRightIcon aria-hidden title="Pil mot høyre" fontSize="1.5rem" />
+                        Slik kan du se at et hjelpemiddel er på avtale med NAV
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <Heading level="2" size="small" style={{ marginTop: '4px' }}>
+                    Leverandører
+                  </Heading>
+                  <ul>
+                    <li>
+                      <Link
+                        className="burgermenu-container__link"
+                        as={NextLink}
+                        href="/leverandorer"
+                        onClick={() => setMenuOpen(false)}
+                      >
+                        <ChevronRightIcon aria-hidden title="Pil mot høyre" fontSize="1.5rem" />
+                        Leverandøroversikt
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </HStack>
               <Heading level="2" size="small" style={{ marginTop: '4px' }}>
                 Hjelpemidler på avtale med NAV
               </Heading>
-              <ul className="burgermenu-container__agreemment-list">
+              <ul>
                 {sortedAgreements.map((agreement) => (
                   <li key={agreement.id}>
                     <Link
@@ -118,7 +122,7 @@ const BurgerMenuContent = ({ searchOpen, menuOpen, setMenuOpen, setSearchOpen }:
                         logNavigationEvent('meny', 'hurtigoversikt', agreement.label)
                       }}
                     >
-                      <ChevronRightIcon title="Pil mote høyre" fontSize="1.5rem" />
+                      <ChevronRightIcon aria-hidden title="Pil mote høyre" fontSize="1.5rem" />
                       {agreement.label}
                     </Link>
                   </li>
