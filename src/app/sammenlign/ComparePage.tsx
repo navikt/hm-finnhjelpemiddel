ac0cb8806ae038d0b482a5bdf504f05361c40607
Vilde Roksand Fauchald <vilde.fauchald@bekk.no>
2024-03-25 19:56:39 +0100

    make table scrollable in both directions

--- src/app/sammenlign/ComparePage.tsx
+++ src/app/sammenlign/ComparePage.tsx
@@ -10,9 +10,10 @@ import { CompareMenuState, useHydratedCompareStore } from '@/utils/global-state-
 import { Product } from '@/utils/product-util'
 import { findUniqueStringValues, formatAgreementRanks, toValueAndUnit, tryParseNumber } from '@/utils/string-util'
 
-import { BodyLong, ChevronRightIcon, Heading, Loader, Table } from '@/components/aksel-client'
+import { BodyLong, ChevronRightIcon, Heading, Link, Loader, Table } from '@/components/aksel-client'
 import AnimateLayout from '@/components/layout/AnimateLayout'
 import ProductCard from '@/components/ProductCard'
+import { ArrowUndoIcon } from '@navikt/aksel-icons'
 
 export default function ComparePage() {
   const { productsToCompare, setCompareMenuState } = useHydratedCompareStore()
@@ -20,8 +21,7 @@ export default function ComparePage() {
 
   const series = productsToCompare.map((product) => product.id)
 
-  //TODO: error handling
-  const { data, error, isLoading } = useSWR<FetchSeriesResponse>(
+  const { data, isLoading } = useSWR<FetchSeriesResponse>(
     series,
     productsToCompare.length > 0 ? fetchProductsWithVariants : null,
     // Må sendes inn for å fikse typescript feil, open issue her: https://github.com/vercel/swr/issues/2826
@@ -29,6 +29,8 @@ export default function ComparePage() {
   )
 
   const productsToCompareWithVariants: Product[] | undefined = data?.products
+  const sortedProductsToCompare =
+    productsToCompareWithVariants && sortProductsOnAgreementPostAndRank(productsToCompareWithVariants)
 
   const handleClick = (event: any) => {
     event.preventDefault()
@@ -75,13 +77,15 @@ export default function ComparePage() {
             </NextLink>
           </section>
         )}
-        {productsToCompareWithVariants && <CompareTable productsToCompare={productsToCompareWithVariants} />}
+        {sortedProductsToCompare && <CompareTable productsToCompare={sortedProductsToCompare} />}
       </div>
     </AnimateLayout>
   )
 }
 
 const CompareTable = ({ productsToCompare }: { productsToCompare: Product[] }) => {
+  const router = useRouter()
+
   const allDataKeysVariants = [
     ...new Set(
       productsToCompare.flatMap((product) => product.variants.flatMap((variant) => Object.keys(variant.techData)))
@@ -129,52 +133,78 @@ const CompareTable = ({ productsToCompare }: { productsToCompare: Product[] }) =
   )
 
   return (
-    <section className="comparing-table">
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader></Table.ColumnHeader>
-            {productsToCompare.map((product) => (
-              <Table.ColumnHeader key={'id-' + product.id}>
-                <ProductCard product={product} type="removable" />
-              </Table.ColumnHeader>
-            ))}
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          <Table.Row>
-            <Table.HeaderCell>Rangering</Table.HeaderCell>
-            {productsToCompare.map((product) => {
-              return <Table.DataCell key={product.id}>{formatAgreementRanks(product.agreements || [])}</Table.DataCell>
-            })}
-          </Table.Row>
-          <Table.Row>
-            <Table.HeaderCell>Antall varianter</Table.HeaderCell>
-            {productsToCompare.map((product) => (
-              <Table.DataCell key={product.id}>{product.variantCount}</Table.DataCell>
-            ))}
-          </Table.Row>
-
-          <Table.Row>
-            <Table.DataCell>
-              <Heading level="2" size="medium">
-                Tekniske egenskaper
-              </Heading>
-            </Table.DataCell>
-            {productsToCompare.length > 1 && <Table.DataCell colSpan={productsToCompare.length + 1}></Table.DataCell>}
-          </Table.Row>
-
-          {allDataKeysVariants.map((key, i) => (
-            <Table.Row key={i}>
-              <Table.HeaderCell>{key}</Table.HeaderCell>
+    <section>
+      <Link as={NextLink} href="" onClick={() => router.back()}>
+        <ArrowUndoIcon title="a11y-title" fontSize="1.5rem" />
+        Legg til flere produkter
+      </Link>
+      <div className="compare-table-container">
+        <Table zebraStripes>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader className="common_headercell"></Table.ColumnHeader>
               {productsToCompare.map((product) => (
-                <Table.DataCell key={key + product.id}>{productRowKeyValue[product.id][key]}</Table.DataCell>
+                <Table.ColumnHeader className="header" key={'id-' + product.id}>
+                  <ProductCard product={product} type="removable" />
+                </Table.ColumnHeader>
               ))}
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.HeaderCell className="side_header">Rangering</Table.HeaderCell>
+              {productsToCompare.map((product) => {
+                return (
+                  <Table.DataCell key={product.id}>{formatAgreementRanks(product.agreements || [])}</Table.DataCell>
+                )
+              })}
+            </Table.Row>
+            <Table.Row>
+              <Table.HeaderCell className="side_header">Antall varianter</Table.HeaderCell>
+              {productsToCompare.map((product) => (
+                <Table.DataCell key={product.id}>{product.variantCount}</Table.DataCell>
+              ))}
+            </Table.Row>
+            <Table.Row>
+              <Table.HeaderCell className="side_header">
+                <Heading level="2" size="medium">
+                  Tekniske egenskaper
+                </Heading>
+              </Table.HeaderCell>
+              {productsToCompare.length > 1 && <Table.DataCell colSpan={productsToCompare.length + 1}></Table.DataCell>}
+            </Table.Row>
+
+            {allDataKeysVariants.map((key, i) => (
+              <Table.Row key={i}>
+                <Table.HeaderCell className="side_header">{key}</Table.HeaderCell>
+                {productsToCompare.map((product) => (
+                  <Table.DataCell key={key + product.id}>{productRowKeyValue[product.id][key]}</Table.DataCell>
+                ))}
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
     </section>
   )
+}
+
+function sortProductsOnAgreementPostAndRank(products: Product[]): Product[] {
+  return products.sort((a, b) => {
+    if (a.agreements.length === 0 && b.agreements.length === 0) {
+      return 0
+    } else if (a.agreements.length === 0) {
+      return 1 // Place products without agreements after products with agreements
+    } else if (b.agreements.length === 0) {
+      return -1 // Place products without agreements after products with agreements
+    } else {
+      // Both products have agreements, sort by postNumber, then rank
+      const postNumberComparison = a.agreements[0].postNr - b.agreements[0].postNr
+      if (postNumberComparison !== 0) {
+        return postNumberComparison
+      } else {
+        return a.agreements[0].rank - b.agreements[0].rank
+      }
+    }
+  })
 }
