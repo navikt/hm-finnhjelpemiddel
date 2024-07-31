16439948e4e83551777fedb6882c56a775926f98
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-07-31 16:38:06 +0200

    Support local feature flags

--- src/toggles/toggles.ts
+++ src/toggles/toggles.ts
@@ -1,2 +1,7 @@
 export type ExpectedToggles = (typeof EXPECTED_TOGGLES)[number];
 export const EXPECTED_TOGGLES = ["adminreg.test", "juledekorasjon"] as const;
+
+export const LOCAL_TOGGLES = [{
+  name: 'finnhjelpemiddel.vis-tilbehor-og-reservedel-lister',
+  enabled: true,
+}]
