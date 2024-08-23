011f9c24a545e43d741854492f97c5f13d635779
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2024-08-23 16:39:09 +0200

    Tilbake til avtaler i meny enn så lenge

--- src/app/BurgerMenuContent.tsx
+++ src/app/BurgerMenuContent.tsx
@@ -1,7 +1,11 @@
 import NextLink from 'next/link'
+import { useMemo } from 'react'
+import useSWR from 'swr'
 
+import { agreementHasNoProducts, AgreementLabel, agreementProductsLink } from '@/utils/agreement-util'
 import { logNavigationEvent } from '@/utils/amplitude'
-import { categories } from '@/utils/category-util'
+import { getAgreementLabels } from '@/utils/api-util'
+import { sortAlphabetically } from '@/utils/sort-util'
 import { Heading, HGrid, Link, VStack } from '@navikt/ds-react'
 
 interface Props {
@@ -10,18 +14,47 @@ interface Props {
 }
 
 const BurgerMenuContent = ({ menuOpen, setMenuOpen }: Props) => {
+  const { data: agreements } = useSWR<AgreementLabel[]>('/agreements/_search', getAgreementLabels, {
+    keepPreviousData: true,
+  })
+
+  const sortedAgreements = useMemo(() => {
+    if (!agreements) return []
+    const filteredData = agreements.filter((agreement) => !agreementHasNoProducts(agreement.identifier))
+    // Create a copy of data to avoid modifying it in place
+    filteredData.sort((a, b) => sortAlphabetically(a.label, b.label))
+
+    return filteredData
+  }, [agreements])
+
   return (
     <>
       {menuOpen && (
         <div className="burgermenu-container">
           <div className="burgermenu-container__content main-wrapper--large">
             <HGrid columns={{ xs: 1, md: '4fr 3fr' }} gap={{ xs: '8', md: '16' }}>
-              <div>
+              <VStack gap={{ xs: '1', md: '1' }}>
                 <Heading level="2" size="small">
-                  KATEGORIER
+                  AVTALER
                 </Heading>
+                <ol className="burgermenu-container__category-list">
+                  {sortedAgreements.map((agreement) => (
+                    <li key={agreement.id}>
+                      <Link
+                        as={NextLink}
+                        href={agreementProductsLink(agreement.id)}
+                        onClick={() => {
+                          setMenuOpen(false)
+                          logNavigationEvent('meny', 'hurtigoversikt', agreement.label)
+                        }}
+                      >
+                        {agreement.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ol>
 
-                <ul className="burgermenu-container__category-list">
+                {/* <ul className="burgermenu-container__category-list">
                   {categories.map((category) => (
                     <li key={category.name}>
                       <HGrid gap="6" columns={'50px auto'}>
@@ -39,10 +72,10 @@ const BurgerMenuContent = ({ menuOpen, setMenuOpen }: Props) => {
                       </HGrid>
                     </li>
                   ))}
-                </ul>
-              </div>
+                </ul> */}
+              </VStack>
 
-              <VStack gap={{ xs: '1', md: '5' }}>
+              <VStack gap={{ xs: '1', md: '2' }}>
                 <Heading level="2" size="small">
                   SNARVEIER
                 </Heading>
