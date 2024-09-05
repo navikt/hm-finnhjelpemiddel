3a9921e998e836af2ddb63a997ab4fe058dd9e62
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2024-09-05 16:20:45 +0200

    700 bold font på FinnHjelpemiddel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,7 @@
 import LayoutProvider from '@/app/layoutProvider'
 import { Metadata } from 'next'
-import { Source_Sans_3 } from 'next/font/google'
 
 import '@/styles/globals.scss'
-
-const source = Source_Sans_3({
-  subsets: ['latin'],
-  weight: ['400', '600', '700'],
-  display: 'swap',
-  preload: true,
-})
 
 export const metadata: Metadata = {
   title: 'FinnHjelpemiddel',
@@ -18,7 +10,7 @@ export const metadata: Metadata = {
 
 async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="no" className={source.className}>
+    <html lang="no">
       <body>
         <LayoutProvider>{children}</LayoutProvider>
       </body>
