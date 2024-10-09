3820af4500688c491622aa6c863e554ccb6f997a
marte pedersen <marte.pedersen@nav.no>
2024-10-09 12:32:14 +0200

    godta å rendre lenker i produktbeskrivelse også

--- src/utils/product-util.ts
+++ src/utils/product-util.ts
@@ -351,7 +351,7 @@ export function validateHTML(input: string | undefined): boolean {
   if (!input) {
     return false
   }
-  const allowedTagsRegex = /<\/?(p|br|strong|italic|ul|li|ol)[^>]*>/gi;
+  const allowedTagsRegex = /<\/?(p|br|strong|italic|ul|li|ol|a)[^>]*>/gi
   const allTagsRegex = /<\/?[^>]+(>|$)/g;
 
   const allowedTags = input.match(allowedTagsRegex) || [];
