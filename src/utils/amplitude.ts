ffe74e0f0ab090397fa2cb607ceaa02f5ce1b694
Arshad A. Kamran <arshad.ali.kamran@nav.no>
2024-11-13 10:23:57 +0100

    cleanup

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,51 +1,14 @@
 'use client'
-import * as amplitude from '@amplitude/analytics-browser';
-import { track} from '@amplitude/analytics-browser';
+import * as amplitude from '@amplitude/analytics-browser'
+import { track } from '@amplitude/analytics-browser'
 
 const APP_NAME = 'hm-oversikt'
 const TEAM_NAME = 'teamdigihot'
 const AMPLITUDE_COLLECTION_URL = 'https://amplitude.nav.no/collect-auto'
 
+type LogEvent = (params: { name: string; data?: any }) => void
 
-type LogEvent = (params: { name: string; data?: any }) => void;
-
-let amplitudeLogger: LogEvent | undefined = undefined;
-
-function getApiKeyFromEnvironment() {
-  switch (process.env.BUILD_ENV) {
-    case "prod":
-      return "10798841ebeba333b8ece6c046322d76";
-    case "dev":
-      return "c1c2553d689ba4716c7d7c4410b521f5";
-    case "test":
-      return "mock";
-    default:
-      return "mock";
-  }
-}
-
-/*function getApiKeyFromEnvironment() {
-  switch (process.env.BUILD_ENV) {
-    case "production":
-      return "10798841ebeba333b8ece6c046322d76";
-    case "development":
-      return "c1c2553d689ba4716c7d7c4410b521f5";
-    default:
-      return "mock";
-  }
-}*/
-
-/*const apiKey = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY*/
-
-/*export enum amplitude_taxonomy {
-  SKJEMA_START = 'skjema startet',
-  SKJEMA_ÅPEN = 'skjema åpnet',
-  SKJEMASTEG_FULLFØRT = 'skjemasteg fullført',
-  SKJEMAVALIDERING_FEILET = 'skjemavalidering feilet',
-  SKJEMAINNSENDING_FEILET = 'skjemainnsending feilet',
-  SKJEMA_FULLFØRT = 'skjema fullført',
-  NAVIGERE = 'navigere',
-}*/
+let amplitudeLogger: LogEvent | undefined = undefined
 
 export enum digihot_customevents {
   VISNING_OVERSIKT = 'visning av sider fra hm-oversikt-app',
@@ -57,46 +20,27 @@ export enum digihot_customevents {
 }
 
 export const initAmplitude = () => {
-  const apiKey = getApiKeyFromEnvironment();
-  console.log("apiKey", apiKey)
-  if (apiKey === "mock") {
+  const apiKey =
+    process.env.BUILD_ENV === 'prod'
+      ? '10798841ebeba333b8ece6c046322d76'
+      : process.env.BUILD_ENV === 'dev'
+        ? 'c1c2553d689ba4716c7d7c4410b521f5'
+        : 'mock'
+  if (apiKey === 'mock') {
     amplitudeLogger = (params: { name: string; data?: any }) => {
-/*      // eslint-disable-next-line no-console*/
-      console.log("[Mock Amplitude Event]", {
+            // eslint-disable-next-line no-console
+      console.log('[Mock Amplitude Event]', {
         name: params.name,
         data: {
-          ...("data" in params.data ? params.data.data : {}),
+          ...('data' in params.data ? params.data.data : {}),
           ...params.data,
         },
-      });
-    };
+      })
+    }
   } else {
     amplitude.init(apiKey!, {
       serverUrl: AMPLITUDE_COLLECTION_URL,
-      serverZone: "EU",
-      autocapture: {
-        attribution: true,
-        pageViews: true,
-        sessions: true,
-        formInteractions: true,
-        fileDownloads: true,
-        elementInteractions: true,
-      },
-    });
-    amplitudeLogger = (params: { name: string; data?: any }) => {
-      amplitude.logEvent(params.name, params.data);
-    };
-  }
-}
-
-
-
-/*export const initAmplitude = (): boolean => {
-  const apiKey = getApiKeyFromEnvironment();
-  try {
-    console.log("initAmplitude called"); // New log statement
-    init(apiKey, undefined, {
-      serverZone: "EU",
+      serverZone: 'EU',
       autocapture: {
         attribution: true,
         pageViews: true,
@@ -106,43 +50,21 @@ export const initAmplitude = () => {
         elementInteractions: true,
       },
     })
-    console.log("Amplitude initialized")
-    return true
-  } catch (e) {
-    console.error("Error initializing Amplitude", e)
-    return false
+    amplitudeLogger = (params: { name: string; data?: any }) => {
+      amplitude.logEvent(params.name, params.data)
+    }
   }
-}*/
-
-/*export const initAmplitude = () => {
-
-  if (amplitude && apiKey) {
-    amplitude.init(apiKey, undefined, {
-/!*      serverUrl: process.env.NEXT_PUBLIC_AMPLITUDE_SERVER_URL,*!/
-      serverUrl: 'https://amplitude.nav.no/collect-auto',
-      ingestionMetadata: {
-        sourceName: window.location.toString()
-      }
-    });
-/!*    amplitude.getInstance().init('default', '', {
-      apiEndpoint: 'amplitude.nav.no/collect-auto',
-      saveEvents: false,
-      includeUtm: true,
-      includeReferrer: true,
-      platform: window.location.toString(),
-    })*!/
-  }
-}*/
+}
 
 export function logAmplitudeEvent(eventName: string, data?: any) {
   setTimeout(() => {
     data = {
-      app: APP_NAME ,
+      app: APP_NAME,
       team: TEAM_NAME,
-      ...data
+      ...data,
     }
     try {
-      if (amplitude)  {
+      if (amplitude) {
         track(eventName, data)
       }
     } catch (error) {
