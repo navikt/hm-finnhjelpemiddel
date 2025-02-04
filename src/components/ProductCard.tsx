487c47b19aeada241a7eda0f38c41485e3408831
marte pedersen <marte.pedersen@nav.no>
2025-02-04 13:19:39 +0100

    Revert "refactor: produktkort vist på rammeavtalesiden (new + no-picture) dratt ut til egene komponent"
    This reverts commit f114de10b894ea7871dce70eb0ca72082a0c9604.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,8 +2,8 @@
 
 import { useHydratedCompareStore } from '@/utils/global-state-util'
 import { Product } from '@/utils/product-util'
-import { MultiplyIcon, PackageIcon } from '@navikt/aksel-icons'
-import { BodyLong, BodyShort, Box, Button, Detail, Heading, HGrid, Link, VStack } from '@navikt/ds-react'
+import { ArrowsSquarepathIcon, MultiplyIcon, PackageIcon } from '@navikt/aksel-icons'
+import { BodyLong, BodyShort, Box, Button, Detail, Heading, HGrid, HStack, Link, VStack } from '@navikt/ds-react'
 import classNames from 'classnames'
 import NextLink from 'next/link'
 import { useSearchParams } from 'next/navigation'
@@ -23,7 +23,7 @@ const ProductCard = ({
   handleIsoButton,
   handleCompareClick,
 }: {
-  type: 'removable' | 'checkbox' | 'plain' | 'large-with-checkbox' | 'horizontal'
+  type: 'removable' | 'checkbox' | 'plain' | 'no-picture' | 'large-with-checkbox' | 'horizontal' | 'new'
   product: Product
   linkOverwrite?: string
   rank?: number
@@ -52,8 +52,12 @@ const ProductCard = ({
     cardClassName = 'product-card--checkbox'
   } else if (type === 'removable') {
     cardClassName = 'product-card--removable'
+  } else if (type === 'no-picture') {
+    cardClassName = 'product-card--no-picture'
   } else if (type === 'horizontal') {
     cardClassName = 'product-card--horizontal'
+  } else if (type === 'new') {
+    cardClassName = 'product-card--new'
   }
 
   const viewHmsOrCount = (
@@ -66,6 +70,36 @@ const ProductCard = ({
       )}
     </>
   )
+
+  if (type === 'no-picture') {
+    return (
+      <Box
+        paddingInline="2"
+        paddingBlock="1"
+        className={classNames('product-card--no-picture', { 'product-card__checked': isInProductsToCompare })}
+      >
+        <VStack gap="1" className="product-card__content">
+          <HStack justify={'space-between'}>
+            {viewHmsOrCount}
+            <CompareCheckbox product={product} handleCompareClick={handleCompareClick} />
+          </HStack>
+          <Detail textColor="subtle">{product.supplierName}</Detail>
+
+          <Link
+            className="product-card__link"
+            href={linkToProduct}
+            aria-label={`Gå til ${product.title}`}
+            as={NextLink}
+            onClick={() => logNavigationEvent('Produktkort', 'produkt', product.title)}
+          >
+            <BodyShort size="small" className="text-line-clamp">
+              {rank && rank < 90 ? `${rank}: ${product.title}` : `${product.title}`}
+            </BodyShort>
+          </Link>
+        </VStack>
+      </Box>
+    )
+  }
 
   if (type === 'horizontal') {
     return (
@@ -89,6 +123,59 @@ const ProductCard = ({
             <BodyLong size="small" className="product-card__product-description">
               {product.attributes.text}
             </BodyLong>
+          </VStack>
+        </HGrid>
+      </Box>
+    )
+  }
+
+  if (type === 'new') {
+    return (
+      <Box padding={{ xs: '3', md: '5' }} className="product-card--new">
+        <HGrid columns={'minmax(0,5fr) minmax(0,3fr)'} gap="5" maxWidth={'370px'} width={'100%'}>
+          <VStack gap="3" width={'100%'}>
+            <BodyShort
+              size="small"
+              weight="semibold"
+              className={
+                onAgreement ? 'product-card__nav-on-agreement-text' : 'product-card__nav-not-on-agreement-text'
+              }
+            >
+              {onAgreement
+                ? currentRank < 90
+                  ? `Nav - Rangering ${currentRank}`
+                  : 'Nav - På avtale'
+                : 'Ikke på avtale'}
+            </BodyShort>
+
+            <Link
+              className="product-card__link-2"
+              href={linkToProduct}
+              aria-label={`Gå til ${product.title}`}
+              as={NextLink}
+              onClick={() => logNavigationEvent('Produktkort', 'produkt', product.title)}
+            >
+              <BodyShort weight="semibold" className="product-card__title">
+                {product.title}
+              </BodyShort>
+            </Link>
+
+            <BodyShort size="small">{product.supplierName}</BodyShort>
+
+            <>
+              {hmsNumbers && hmsNumbers?.length === 1 && (
+                <BodyShort size="small" className="product-card__hms-numbers">
+                  {hmsNumbers.join(', ')}
+                </BodyShort>
+              )}
+              {((variantCount && hmsNumbers && hmsNumbers?.length > 1) || (variantCount && !hmsNumbers)) && (
+                <BodyShort size="small">{`${variantCount} ${variantCount > 1 ? 'varianter' : 'variant'}`} </BodyShort>
+              )}
+            </>
+          </VStack>
+          <VStack align="center" justify="space-between" gap="2" width={'100%'}>
+            <ProductImage src={firstImageSrc} productTitle={product.title} />
+            <CompareButton product={product} handleCompareClick={handleCompareClick} />
           </VStack>
         </HGrid>
       </Box>
@@ -144,6 +231,45 @@ const ProductCard = ({
         <ProductImage src={firstImageSrc} productTitle={product.title} />
       </VStack>
     </Box>
+  )
+}
+
+const CompareButton = ({
+  product,
+  handleCompareClick,
+}: {
+  product: Product
+  handleCompareClick: (() => void) | undefined
+}) => {
+  const { setProductToCompare, removeProduct, productsToCompare } = useHydratedCompareStore()
+
+  const toggleCompareProduct = () => {
+    handleCompareClick && handleCompareClick()
+    productsToCompare.filter((procom: Product) => product.id === procom.id).length === 1
+      ? removeProduct(product.id)
+      : setProductToCompare(product)
+  }
+
+  const isInProductsToCompare = productsToCompare.filter((procom: Product) => product.id === procom.id).length >= 1
+
+  return (
+    <Button
+      className={classNames('product-card__compare-button', {
+        'product-card__compare-button--checked': isInProductsToCompare,
+      })}
+      size="small"
+      variant="secondary"
+      value="Legg produktet til sammenligning"
+      onClick={toggleCompareProduct}
+      // checked={isInProductsToCompare}
+      icon={<ArrowsSquarepathIcon aria-hidden />}
+      iconPosition="left"
+      aria-pressed={isInProductsToCompare}
+    >
+      <div aria-label={`sammenlign ${product.title}`}>
+        <span aria-hidden>Sammenlign</span>
+      </div>
+    </Button>
   )
 }
 
