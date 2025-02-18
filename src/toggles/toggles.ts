09d6445fef9db755cc5318d6f35cdb3f2c4b51a5
marte pedersen <marte.pedersen@nav.no>
2025-02-18 14:31:31 +0100

    skrur av pepperkaka lokalt

--- src/toggles/toggles.ts
+++ src/toggles/toggles.ts
@@ -1,13 +1,22 @@
-export type ExpectedToggles = (typeof EXPECTED_TOGGLES)[number];
-export const EXPECTED_TOGGLES = ["adminreg.test", "juledekorasjon", "finnhjelpemiddel.vis-tilbehor-og-reservedel-lister", "finnhjelpemiddel.feilbanner"] as const;
+export type ExpectedToggles = (typeof EXPECTED_TOGGLES)[number]
+export const EXPECTED_TOGGLES = [
+  'adminreg.test',
+  'juledekorasjon',
+  'finnhjelpemiddel.vis-tilbehor-og-reservedel-lister',
+  'finnhjelpemiddel.feilbanner',
+] as const
 
-export const LOCAL_TOGGLES = [{
-  name: 'finnhjelpemiddel.vis-tilbehor-og-reservedel-lister',
-  enabled: true,
-}, {
-  name: 'juledekorasjon',
-  enabled: true,
-}, {
-  name: 'finnhjelpemiddel.feilbanner',
-  enabled: false,
-}]
+export const LOCAL_TOGGLES = [
+  {
+    name: 'finnhjelpemiddel.vis-tilbehor-og-reservedel-lister',
+    enabled: true,
+  },
+  {
+    name: 'juledekorasjon',
+    enabled: false,
+  },
+  {
+    name: 'finnhjelpemiddel.feilbanner',
+    enabled: false,
+  },
+]
