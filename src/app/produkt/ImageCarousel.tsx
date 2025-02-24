ee85d11a047978c1c8a27c300c8da245c84f5563
Atle Nordland <atle.nordland@nav.no>
2025-02-24 13:09:17 +0100

    thumbsnails og nesteknapper

--- src/app/produkt/ImageCarousel.tsx
+++ src/app/produkt/ImageCarousel.tsx
@@ -3,9 +3,12 @@
 import { Photo } from '@/utils/product-util'
 import useEmblaCarousel from 'embla-carousel-react'
 import styles from './ImageCarousel.module.scss'
-import { useCallback, useEffect, useState } from 'react'
+import React, { ComponentPropsWithRef, useCallback, useEffect, useState } from 'react'
 import Image from 'next/image'
 import { largeImageLoader, smallImageLoader } from '@/utils/image-util'
+import { HGrid, HStack, VStack } from '@navikt/ds-react'
+import { EmblaCarouselType } from 'embla-carousel'
+import classNames from 'classnames'
 
 const ImageCarousel = ({ images }: { images: Photo[] }) => {
   const [selectedIndex, setSelectedIndex] = useState(0)
@@ -36,33 +39,32 @@ const ImageCarousel = ({ images }: { images: Photo[] }) => {
     emblaMainApi.on('select', onSelect).on('reInit', onSelect)
   }, [emblaMainApi, onSelect])
 
+  const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaMainApi)
+
   return (
-    <section className={styles.embla}>
+    <VStack gap={'4'} className={styles.embla}>
       <div className={styles.embla__viewport} ref={emblaMainRef}>
         <div className={styles.embla__container}>
           {images.map((image, index) => (
-            <div className={styles.embla__slide} key={index}>
-              <div className={styles.embla__slide__number}>
-                {
-                  <Image
-                    aria-selected={true}
-                    //onClick={() => setActive(i)}
-                    loader={largeImageLoader}
-                    src={image.uri}
-                    alt={`Produktbilde ${index + 1} av ${images.length}`}
-                    fill
-                    className={styles.image}
-                  />
-                }
-              </div>
+            <div className={styles.emblaSlide} key={index}>
+              {
+                <Image
+                  aria-selected={true}
+                  loader={largeImageLoader}
+                  src={image.uri}
+                  alt={`Produktbilde ${index + 1} av ${images.length}`}
+                  fill
+                  className={styles.image}
+                />
+              }
             </div>
           ))}
         </div>
       </div>
-
-      <div className={styles.emblaThumbs}>
+      <HGrid columns={'1fr 9fr 1fr'}>
+        <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
         <div className={styles.emblaThumbs__viewport} ref={emblaThumbsRef}>
-          <div className={styles.emblaThumbs__container}>
+          <HStack wrap={false} gap={'2'}>
             {images.map((image, index) => (
               <Thumb
                 imageUri={image.uri}
@@ -72,10 +74,11 @@ const ImageCarousel = ({ images }: { images: Photo[] }) => {
                 index={index}
               />
             ))}
-          </div>
+          </HStack>
         </div>
-      </div>
-    </section>
+        <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+      </HGrid>
+    </VStack>
   )
 }
 
@@ -91,19 +94,98 @@ const Thumb = ({
   onClick: () => void
 }) => {
   return (
-    <div className={styles.emblaThumbs__slide}>
-      <button onClick={onClick} type="button" className={styles.emblaThumbs__slide__number}>
+    <button
+      onClick={onClick}
+      type="button"
+      className={classNames(styles.emblaThumbs__slide__number, { [styles.emblaThumbs__slideSelected]: selected })}
+    >
+      <div className={styles.thumbImageWrapper}>
         <Image
           aria-selected={true}
           //onClick={() => setActive(i)}
-          loader={largeImageLoader}
+          loader={smallImageLoader}
           src={imageUri}
           alt={`Produktbilde ${index + 1}`}
-          width={50}
-          height={50}
+          fill
+          className={styles.thumbImage}
         />
-      </button>
-    </div>
+      </div>
+    </button>
+  )
+}
+
+type UsePrevNextButtonsType = {
+  prevBtnDisabled: boolean
+  nextBtnDisabled: boolean
+  onPrevButtonClick: () => void
+  onNextButtonClick: () => void
+}
+
+export const usePrevNextButtons = (emblaApi: EmblaCarouselType | undefined): UsePrevNextButtonsType => {
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true)
+
+  const onPrevButtonClick = useCallback(() => {
+    if (!emblaApi) return
+    emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const onNextButtonClick = useCallback(() => {
+    if (!emblaApi) return
+    emblaApi.scrollNext()
+  }, [emblaApi])
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setPrevBtnDisabled(!emblaApi.canScrollPrev())
+    setNextBtnDisabled(!emblaApi.canScrollNext())
+  }, [])
+
+  useEffect(() => {
+    if (!emblaApi) return
+
+    onSelect(emblaApi)
+    emblaApi.on('reInit', onSelect).on('select', onSelect)
+  }, [emblaApi, onSelect])
+
+  return {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  }
+}
+
+type PropType = ComponentPropsWithRef<'button'>
+
+export const PrevButton: React.FC<PropType> = (props) => {
+  const { children, ...restProps } = props
+
+  return (
+    <button className={styles.embla__button} type="button" {...restProps}>
+      <svg className={styles.embla__button__svg} viewBox="0 0 532 532">
+        <path
+          fill="currentColor"
+          d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
+        />
+      </svg>
+      {children}
+    </button>
+  )
+}
+
+export const NextButton: React.FC<PropType> = (props) => {
+  const { children, ...restProps } = props
+
+  return (
+    <button className={styles.embla__button} type="button" {...restProps}>
+      <svg className={styles.embla__button__svg} viewBox="0 0 532 532">
+        <path
+          fill="currentColor"
+          d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
+        />
+      </svg>
+      {children}
+    </button>
   )
 }
 
