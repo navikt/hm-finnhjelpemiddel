e7ccdcb55fd5fcfa49a489b529aaf8639092882a
marte pedersen <marte.pedersen@nav.no>
2025-03-17 15:05:49 +0100

    aner ikke hva jeg driver med ;_;

---
+++ src/app/produkt/variants/FilterTull.tsx
@@ -0,0 +1,93 @@
+import { Button, HStack, Select, VStack } from '@navikt/ds-react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useCallback } from 'react'
+import styles from './FilterTull.module.scss'
+
+type Props = {
+  rows: { [key: string]: string[] }
+  filterNames: string[]
+}
+
+export const FilterTull = ({ rows, filterNames }: Props) => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const filters: { [key: string]: string[] } = Object.assign(
+    {},
+    ...filterNames.map((name) => ({
+      [name]: Array.from(
+        new Set(
+          Object.entries(rows)
+            .filter(([key, _]) => key.startsWith(name))
+            .flatMap(([_, value]) => value)
+        )
+      ).sort(),
+    }))
+  )
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString())
+      if (value.length === 0) {
+        params.delete(name)
+      } else {
+        params.set(name, value)
+      }
+
+      return params.toString()
+    },
+    [searchParams]
+  )
+
+  const onSelectFilter = (name: string, value: string) => {
+    const newSearchParams = createQueryString(name, value)
+    router.replace(`${pathname}?${newSearchParams}`, { scroll: false })
+  }
+
+  const resetFilterAll = () => {
+    router.replace(`${pathname}`, { scroll: false })
+  }
+
+  return (
+    <VStack gap={'4'}>
+      <HStack gap={'4'} width={'fit-content'} maxWidth={'40%'}>
+        <SeteSelect filters={filters} selectFilter={onSelectFilter} />
+      </HStack>
+      <Button size={'small'} onClick={resetFilterAll} className={styles.fjernButton}>
+        Fjern alle filtre
+      </Button>
+    </VStack>
+  )
+}
+
+const SeteSelect = ({
+  filters,
+  selectFilter,
+}: {
+  filters: { [_: string]: string[] }
+  selectFilter: (name: string, value: string) => void
+}) => {
+  const searchParams = useSearchParams()
+  return (
+    <>
+      {Object.entries(filters).map(([key, row], index) => {
+        return (
+          <Select
+            key={index + 1}
+            label={key}
+            onChange={(event) => selectFilter(key, event.target.value)}
+            value={searchParams.get(key) ?? ''}
+          >
+            <option key="" value="">
+              Velg
+            </option>
+            {row.map((value, i) => (
+              <option key={i + 1}>{value}</option>
+            ))}
+          </Select>
+        )
+      })}
+    </>
+  )
+}
--- src/app/produkt/variants/VariantView.tsx
+++ src/app/produkt/variants/VariantView.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useRef, useState } from 'react'
 import { ArrowDownIcon, ArrowsUpDownIcon, ArrowUpIcon, TabsIcon } from '@navikt/aksel-icons'
 import { Alert, Box, Heading, Table } from '@navikt/ds-react'
-import { fetchProducts } from '@/utils/api-util'
 import { mapSearchParams } from '@/utils/mapSearchParams'
 import { useSearchParams } from 'next/navigation'
 import { customSort, hasDifferentValues, sortColumnsByRowKey } from '@/app/produkt/variants/variant-utils'
@@ -14,17 +13,24 @@ import { VariantSupplierRefRow } from '@/app/produkt/variants/VariantSupplierRef
 import { VariantRankRow } from '@/app/produkt/variants/VariantRankRowProps'
 import { VariantPostRow } from '@/app/produkt/variants/VariantPostRow'
 import { VariantTechnicalDataRow } from '@/app/produkt/variants/VariantTechnicalDataRow'
-import { Product } from '@/utils/product-util'
+import { Product, ProductVariant } from '@/utils/product-util'
 import { toValueAndUnit } from '@/utils/string-util'
 import NextLink, { default as Link } from 'next/link'
-import useSWR from 'swr'
 import { VariantFilters } from '@/app/produkt/variants/VariantFilters'
 import { SharedVariantDataTable } from '@/app/produkt/variants/SharedVariantDataTable'
 import { SingleVariantTable } from '@/app/produkt/variants/SingleVariantTable'
+import { FilterTull } from '@/app/produkt/variants/FilterTull'
 
 export type SortColumns = {
   orderBy: string | null
   direction: 'ascending' | 'descending'
+}
+
+type KeyValArray = {
+  [x: string]: {
+    value: string
+    unit: string
+  }
 }
 
 export const VariantView = ({ product }: { product: Product }) => {
@@ -34,6 +40,8 @@ export const VariantView = ({ product }: { product: Product }) => {
   const variantNameElementRef = useRef<HTMLTableCellElement>(null)
   const [variantNameElementHeight, setVariantNameElementHeight] = useState(0)
   const [selectedColumn, setSelectedColumn] = useState<string | null>(null)
+
+  const [filteredVariants, setFilteredVariants] = useState<ProductVariant[]>(product.variants)
 
   const handleColumnClick = (columnKey: string) => {
     setSelectedColumn(columnKey)
@@ -55,40 +63,58 @@ export const VariantView = ({ product }: { product: Product }) => {
     .flatMap((variant) => [variant.supplierRef?.toLocaleLowerCase()])
     .includes(searchData.searchTerm?.toLowerCase())
 
-  const { data: dataAndFilter } = useSWR(
-    { from: 0, size: 150, searchData: { ...searchData, searchTerm: '' }, dontCollapse: true, seriesId: product.id },
-    fetchProducts,
-    { keepPreviousData: true }
-  )
+  const setedybde = 'Setedybde'
+  const setebredde = 'Setebredde'
+  const setehoyde = 'Setehøyde'
 
-  const productWithFilteredVariants = dataAndFilter && dataAndFilter.products
+  const filterNames = [setedybde, setebredde, setehoyde]
 
-  const productVariantsToShow = productWithFilteredVariants
-    ? productWithFilteredVariants.length > 0
-      ? searchTermMatchesHms
-        ? productWithFilteredVariants[0].variants.filter((variant) => variant.hmsArtNr === searchData.searchTerm)
-        : searchTermMatchesSupplierRef
-          ? productWithFilteredVariants[0].variants.filter((variant) => variant.supplierRef === searchData.searchTerm)
-          : productWithFilteredVariants[0].variants
-      : []
-    : product.variants
+  const mapSearch = (ja: string): { value: string; unit: string } => {
+    const testen = ja.split(' ')
+    return { value: testen[0], unit: testen[1] }
+  }
+  useEffect(() => {
+    const availableFilters: KeyValArray = Object.assign(
+      {},
+      ...searchParams
+        .entries()
+        .toArray()
+        .filter(([key, _]) => filterNames.includes(key))
+        .map(([key, value]) => ({ [key]: mapSearch(value) }))
+    )
+
+    const testVars = Object.entries(availableFilters)
+      .flatMap(([rootKey, rootValue]) =>
+        product.variants.filter((variant) =>
+          Object.entries(variant.techData).some(
+            ([key, value]) => key.startsWith(rootKey) && value.unit === rootValue.unit && value.value >= rootValue.value
+          )
+        )
+      )
+      .filter((variant) => (searchTermMatchesSupplierRef ? variant.supplierRef === searchData.searchTerm : true))
+      .filter((variant) => (searchTermMatchesHms ? variant.hmsArtNr === searchData.searchTerm : true))
+
+    console.log('testVars', testVars)
+    console.log('availableFilters', availableFilters)
+    setFilteredVariants(Object.entries(availableFilters).length === 0 ? product.variants : testVars)
+  })
 
   useEffect(() => {
     if (variantNameElementRef.current) {
       setVariantNameElementHeight(variantNameElementRef.current.offsetHeight)
     }
-  }, [productVariantsToShow])
+  }, [filteredVariants])
 
-  const sortedByKey = sortColumnsByRowKey(productVariantsToShow, sortColumns)
-  const allDataKeys =
+  const sortedByKey = sortColumnsByRowKey(filteredVariants, sortColumns)
+  const allTechnicalDataKeys =
     product.isoCategory === '18301505'
       ? [...new Set(sortedByKey.flatMap((variant) => Object.keys(variant.techData)))].sort(customSort)
       : [...new Set(sortedByKey.flatMap((variant) => Object.keys(variant.techData)))].sort()
 
   const rows: { [key: string]: string[] } = Object.assign(
     {},
-    ...allDataKeys.map((key) => ({
-      [key]: productVariantsToShow.map((variant) =>
+    ...allTechnicalDataKeys.map((key) => ({
+      [key]: filteredVariants.map((variant) =>
         variant.techData[key] !== undefined
           ? toValueAndUnit(variant.techData[key].value, variant.techData[key].unit)
           : '-'
@@ -98,7 +124,7 @@ export const VariantView = ({ product }: { product: Product }) => {
 
   const commonDataRows: { [key: string]: string } = Object.assign(
     {},
-    ...allDataKeys
+    ...allTechnicalDataKeys
       .filter((key) =>
         product.variants.every(
           (variant) => variant.techData[key] && variant.techData[key].value === product.variants[0].techData[key].value
@@ -167,17 +193,19 @@ export const VariantView = ({ product }: { product: Product }) => {
           </Heading>
           <Box paddingBlock="4">
             <VariantFilters product={product} />
+            <FilterTull rows={rows} filterNames={filterNames} />
+
             <Heading level="3" size="small" className="spacing-vertical--small">
-              {`${productVariantsToShow.length} av ${product.variantCount} varianter:`}
+              {`${filteredVariants.length} av ${product.variantCount} varianter:`}
             </Heading>
 
-            {productVariantsToShow.length === 0 && (
+            {filteredVariants.length === 0 && (
               <Alert variant="warning" className="spacing-top--small">
                 Ingen av variantene passer med filteret ditt
               </Alert>
             )}
 
-            {productVariantsToShow.length > 0 && (
+            {filteredVariants.length > 0 && (
               <>
                 <div className="variants-table" id="variants-table">
                   <Table zebraStripes>
