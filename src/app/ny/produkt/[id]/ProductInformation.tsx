81a441c7a1453d3e74aa47454886e92a2b51f90b
marte pedersen <marte.pedersen@nav.no>
2025-04-02 16:38:43 +0200

    dus bord på trekkspillet

--- src/app/ny/produkt/[id]/ProductInformation.tsx
+++ src/app/ny/produkt/[id]/ProductInformation.tsx
@@ -3,8 +3,12 @@ import { Product } from '@/utils/product-util'
 import { GeneralProductInformation } from '@/app/ny/produkt/[id]/GeneralProductInformation'
 import { Videos } from '@/app/ny/produkt/[id]/Videos'
 import { Documents } from '@/app/ny/produkt/[id]/Documents'
+import styles from './ProductInformation.module.scss'
+import { useState } from 'react'
 
 export const ProductInformation = ({ product }: { product: Product }) => {
+  const [open, setOpen] = useState<boolean>(false)
+
   return (
     <VStack gap={'2'}>
       <Heading size={'medium'} level={'2'}>
@@ -12,19 +16,19 @@ export const ProductInformation = ({ product }: { product: Product }) => {
       </Heading>
       <Accordion headingSize={'small'}>
         <Accordion.Item defaultOpen>
-          <Accordion.Header>Generell informasjon</Accordion.Header>
+          <Accordion.Header className={styles.accordion}>Generell informasjon</Accordion.Header>
           <Accordion.Content>
             <GeneralProductInformation product={product} />
           </Accordion.Content>
         </Accordion.Item>
         <Accordion.Item>
-          <Accordion.Header>Video</Accordion.Header>
+          <Accordion.Header className={styles.accordion}>Video</Accordion.Header>
           <Accordion.Content>
             <Videos videos={product.videos} />
           </Accordion.Content>
         </Accordion.Item>
-        <Accordion.Item>
-          <Accordion.Header>Dokumenter</Accordion.Header>
+        <Accordion.Item className={styles.accordionLast} onOpenChange={() => setOpen(!open)}>
+          <Accordion.Header className={open ? styles.accordion : styles.accordionLast}>Dokumenter</Accordion.Header>
           <Accordion.Content>
             <Documents documents={product.documents} />
           </Accordion.Content>
