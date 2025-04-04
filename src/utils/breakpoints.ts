186adc04a7dff4b8ddb9cd4189761fe98a28f444
marte pedersen <marte.pedersen@nav.no>
2025-04-04 14:22:02 +0200

    fjerner ubrukt kode

--- src/utils/breakpoints.ts
+++
@@ -1,4 +0,0 @@
-//Breakpoints for styled components
-export const maxWidthPhoneOnly = 549 + 'px'
-export const minWidthTabletUp = 550 + 'px'
-export const minWidthDesktopUp = 1024 + 'px'
--- src/utils/type-util.ts
+++
@@ -1,2 +0,0 @@
-export type AtLeastOne<T> = { [K in keyof T]: Pick<T, K> }[keyof T]
-export type Entries<T> = { [K in keyof T]: [key: K, value: T[K]] }[keyof T][]
