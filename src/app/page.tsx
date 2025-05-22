b0fb6f1e9bbe37ef8582e04d099a576da5703b22
Atle Nordland <atle.nordland@nav.no>
2025-05-22 11:04:11 +0200

    Ny forside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,16 @@ import { usePathname, useRouter } from 'next/navigation'
 import { useCallback, useEffect } from 'react'
 
 import AutocompleteSearch from '@/components/AutocompleteSearch'
-import NewsList from '@/components/NewsList'
 import { logNavigationEvent, logVisit } from '@/utils/amplitude'
-import {
-  BicycleIcon,
-  Chat2Icon,
-  EarIcon,
-  EyeIcon,
-  HatSchoolIcon,
-  LocationPinIcon,
-  WheelchairIcon,
-} from '@navikt/aksel-icons'
-import { Bleed, BodyLong, Box, Heading, HGrid, Hide, HStack, Link, Show, VStack } from '@navikt/ds-react'
+import { Bleed, Box, Heading, HGrid, VStack } from '@navikt/ds-react'
+import KontaktOss from '@/app/forside/KontaktOss'
+import FinnHjelpemiddelLogo from '@/app/forside/FinnHjelpemiddelLogo'
+import Agreements from '@/app/forside/Agreements'
+import { NewsFeed } from '@/app/forside/NewsFeed'
+import styles from './FrontPage.module.scss'
+import { OtherAgreements } from '@/app/forside/OtherAgreements'
 
-import AgreementList from './rammeavtale/AgreementList'
-import { InformationNavLinks } from '@/app/InformationNavLinks'
-
-function Home() {
+function FrontPage() {
   const path = usePathname()
   const router = useRouter()
 
@@ -52,103 +45,41 @@ function Home() {
   )
 
   return (
-    <div className="home-page">
-      <VStack className="main-wrapper--large" gap={{ xs: '12', md: '24' }}>
-        <Bleed
-          marginInline="full"
-          reflectivePadding
-          style={{ backgroundColor: '#FEF5EF' }}
-          className="home-page__heading-and-search-container"
+    <VStack className={styles.container} paddingInline={{ xs: '4', md: '12' }} gap={{ xs: '12', md: '16' }}>
+      <Bleed marginInline="full" reflectivePadding style={{ backgroundColor: '#F5F9FF' }}>
+        <HGrid
+          className={styles.heroContentContainer}
+          columns={{ sm: 1, md: 2 }}
+          align={'center'}
+          gap={'8'}
+          marginBlock={{ xs: '10', md: '20' }}
         >
-          <VStack gap={{ xs: '8', md: '11' }}>
-            <Box
-              paddingInline="10"
-              paddingBlock="4"
-              style={{ backgroundColor: '#FAD8E7' }}
-              width="fit-content"
-              className="home-page__icons-logo"
-            >
-              <HStack gap="4">
-                <WheelchairIcon aria-hidden /> <EyeIcon aria-hidden /> <BicycleIcon aria-hidden />
-                <EarIcon aria-hidden />
-              </HStack>
-            </Box>
-            <VStack gap="2">
-              <Heading level="1" size="xlarge" className="home-page__heading home-page__finnhjelpemiddel">
-                FinnHjelpemiddel
-              </Heading>
-              <Heading level="1" size="large" className="home-page__heading">
-                Her kan du finne hjelpemidler på det norske markedet
-              </Heading>
-            </VStack>
-            <Box maxWidth={'530px'}>
-              <AutocompleteSearch onSearch={onSearch} />
+          <VStack gap={{ xs: '8', md: '11' }} maxWidth={'490px'} style={{ gridArea: 'box1' }}>
+            <Heading level="1" size="large">
+              Her kan du finne hjelpemidler på det norske markedet
+            </Heading>
+            <Box>
+              <AutocompleteSearch onSearch={onSearch} placeholder={'Søk etter hjelpemiddel eller HMS-nummer'} />
             </Box>
           </VStack>
-        </Bleed>
 
-        <HGrid gap={{ xs: '12', md: '14' }} columns={{ xs: '1fr', md: '2fr 1fr' }}>
-          <Show below="md">
-            <InformationNavLinks />
-          </Show>
-          <AgreementList />
-          <VStack gap="18">
-            <Hide below="md">
-              <InformationNavLinks />
-            </Hide>
-            <NewsList />
-          </VStack>
+          <Box className={styles.logoBox} style={{ gridArea: 'box2' }}>
+            <Box width={{ xs: '300px', md: '360px' }}>
+              <FinnHjelpemiddelLogo />
+            </Box>
+          </Box>
         </HGrid>
+      </Bleed>
 
-        <Bleed marginInline="full" asChild reflectivePadding>
-          <div className="home-page__kontakt-oss">
-            <HGrid gap="8" columns={{ xs: 1, md: 3 }}>
-              <HGrid columns={'65px auto'} gap={{ xs: '2', md: '6' }}>
-                <div className="home-page__kontakt-oss-icon">
-                  <Chat2Icon aria-hidden fontSize={'32px'} />
-                </div>
-                <div className="spacing-top--small">
-                  <Heading level="4" size="small" className="spacing-bottom--medium">
-                    <Link href="https://www.nav.no/kontaktoss" className="home-page__link">
-                      Kontakt Nav
-                    </Link>
-                  </Heading>
-                  <BodyLong>Skriv med Chatrobot Frida, skriv til oss, eller ring kontaktsenteret i Nav.</BodyLong>
-                </div>
-              </HGrid>
-              <HGrid columns={'65px auto'} gap={{ xs: '2', md: '6' }}>
-                <div className="home-page__kontakt-oss-icon">
-                  <LocationPinIcon aria-hidden fontSize={'32px'} />
-                </div>
-                <div className="spacing-top--small">
-                  <Heading level="4" size="small" className="spacing-bottom--medium">
-                    <Link href="https://www.nav.no/kontaktoss#finn-hjelpemiddelsentral" className="home-page__link">
-                      Finn din hjelpemiddelsentral
-                    </Link>
-                  </Heading>
-                  <BodyLong>Finn kontaktinformasjon og les om inn- og utlevering av hjelpemidler.</BodyLong>
-                </div>
-              </HGrid>
+      <Agreements />
+      <NewsFeed />
+      <Bleed marginInline="full" reflectivePadding style={{ backgroundColor: '#F5F9FF' }}>
+        <OtherAgreements />
+      </Bleed>
 
-              <HGrid columns={'65px auto'} gap={{ xs: '2', md: '6' }}>
-                <div className="home-page__kontakt-oss-icon">
-                  <HatSchoolIcon aria-hidden fontSize={'32px'} />
-                </div>
-                <div className="spacing-top--small">
-                  <Heading level="4" size="small" className="spacing-bottom--medium">
-                    <Link href="https://www.kunnskapsbanken.net/" className="home-page__link">
-                      Kunnskapsbanken
-                    </Link>
-                  </Heading>
-                  <BodyLong>Fagstoff og kurs om hjelpemidler og tilrettelegging.</BodyLong>
-                </div>
-              </HGrid>
-            </HGrid>
-          </div>
-        </Bleed>
-      </VStack>
-    </div>
+      <KontaktOss />
+    </VStack>
   )
 }
 
-export default Home
+export default FrontPage
