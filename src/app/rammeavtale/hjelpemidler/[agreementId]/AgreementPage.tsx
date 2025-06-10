72d41b474ca5d4346ade134e8cf1433ee3d46b45
Marte Pedersen <Marte.Pedersen@nav.no>
2025-06-10 16:52:23 +0200

    Fancy filter (#714)
    ny filter dropdown basert på actionmenu fra aksel og design fra nye skisser (mer som chips)

--- src/app/rammeavtale/hjelpemidler/[agreementId]/AgreementPage.tsx
+++ src/app/rammeavtale/hjelpemidler/[agreementId]/AgreementPage.tsx
@@ -1,16 +1,14 @@
 'use client'
 
-import { Filter, FilterData, getFiltersAgreement, getProductsOnAgreement } from '@/utils/api-util'
+import { FilterData, getFiltersAgreement, getProductsOnAgreement } from '@/utils/api-util'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useRef, useState } from 'react'
-import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
+import { useCallback, useEffect, useState } from 'react'
 import useSWR from 'swr'
 import CompareMenu from '@/components/layout/CompareMenu'
 import { useFeatureFlags } from '@/hooks/useFeatureFlag'
 import { Agreement, mapAgreementProducts } from '@/utils/agreement-util'
-import { mapSearchParams, toSearchQueryString } from '@/utils/mapSearchParams'
+import { mapSearchParams } from '@/utils/mapSearchParams'
 import { PostBucketResponse } from '@/utils/response-types'
-import { FormSearchData, initialAgreementSearchDataState } from '@/utils/search-state-util'
 import { dateToString } from '@/utils/string-util'
 import {
   ArrowRightIcon,
@@ -31,18 +29,16 @@ import { MobileOverlayModal } from '@/components/MobileOverlayModal'
 import { useMobileOverlayStore } from '@/utils/global-state-util'
 import NextLink from 'next/link'
 import styles from '@/app/rammeavtale/AgreementPage.module.scss'
+import useSWRImmutable from 'swr/immutable'
 
-export type AgreementSearchData = {
-  searchTerm: string
-  hidePictures: boolean
+export type AgreementFilters = {
+  [key in 'leverandor' | 'delkontrakt']: string[]
 }
 
 const AgreementPage = ({ agreement }: { agreement: Agreement }) => {
   const router = useRouter()
   const pathname = usePathname()
   const searchParams = useSearchParams()
-
-  const searchFormRef = useRef<HTMLFormElement>(null)
 
   const { setMobileOverlayOpen } = useMobileOverlayStore()
   const [showSidebar, setShowSidebar] = useState(false)
@@ -56,29 +52,35 @@ const AgreementPage = ({ agreement }: { agreement: Agreement }) => {
     '47105bc7-10a2-48fc-9ff2-95d6e7bb6b96',
   ]
 
-  const formMethods = useForm<FormSearchData>({
-    defaultValues: {
-      hidePictures: 'show-pictures',
-      ...searchData,
-      filters: { ...initialAgreementSearchDataState.filters, ...searchData.filters, status: ['På avtale'] },
-    },
-  })
-
   useEffect(() => {
     setShowSidebar(window.innerWidth >= 1024)
     window.addEventListener('resize', () => setShowSidebar(window.innerWidth >= 1024))
   }, [])
 
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString())
+
+      if (value === '') {
+        params.delete(name)
+      } else if (params.getAll(name).includes(value)) {
+        params.delete(name, value)
+      } else if (params.has(name)) {
+        params.append(name, value)
+      } else {
+        params.set(name, value)
+      }
+
+      return params.toString()
+    },
+    [searchParams]
+  )
+
   const handleShowHidePics = () => {
     const value = pictureToggleValue === 'show-pictures' ? 'hide-pictures' : 'show-pictures'
-    formMethods.setValue('hidePictures', value)
-    searchFormRef.current?.requestSubmit()
-  }
 
-  const onSubmit: SubmitHandler<FormSearchData> = () => {
-    router.replace(`${pathname}?${toSearchQueryString(formMethods.getValues(), searchData.searchTerm)}`, {
-      scroll: false,
-    })
+    const newSearchParams = createQueryString('hidePictures', value)
+    router.replace(`${pathname}?${newSearchParams}`, { scroll: false })
   }
 
   const {
@@ -89,7 +91,7 @@ const AgreementPage = ({ agreement }: { agreement: Agreement }) => {
     keepPreviousData: true,
   })
 
-  const { data: filtersFromData, isLoading: filtersIsLoading } = useSWR<FilterData>(
+  const { data: filtersFromData } = useSWRImmutable<FilterData>(
     { agreementId: agreement.id, type: 'filterdata' },
     getFiltersAgreement,
     {
@@ -97,16 +99,10 @@ const AgreementPage = ({ agreement }: { agreement: Agreement }) => {
     }
   )
 
-  const postFilters: Filter = {
-    values: agreement.posts
-      .filter((post) => post.nr != 99)
-      .sort((a, b) => a.nr - b.nr)
-      .map((post) => ({
-        key: post.title,
-        doc_count: 1,
-        label: post.title,
-      })),
-  }
+  const postFilters: string[] = agreement.posts
+    .filter((post) => post.nr != 99)
+    .sort((a, b) => a.nr - b.nr)
+    .map((post) => post.title)
 
   if (!postBuckets || !filtersFromData) {
     return (
@@ -116,8 +112,10 @@ const AgreementPage = ({ agreement }: { agreement: Agreement }) => {
     )
   }
 
-  const filters: FilterData = {
-    ...filtersFromData,
+  const leverandorFilter: string[] = filtersFromData?.leverandor.values.map((value) => value.key.toString()) || []
+
+  const filters: AgreementFilters = {
+    leverandor: leverandorFilter,
     delkontrakt: postFilters,
   }
 
@@ -125,13 +123,18 @@ const AgreementPage = ({ agreement }: { agreement: Agreement }) => {
   //Dersom det finnes en delkontrakt med over 500 varianter vil ikke alle seriene vises. Da må vi vurdere å ha et kall per delkontrakt.
   const posts = mapAgreementProducts(postBuckets, agreement, searchData.filters)
 
-  const totalProducts = posts
-    .map((post) => post.products.length)
-    .reduce((previousValue, currentValue) => previousValue + currentValue)
+  const totalProducts =
+    posts.length > 0
+      ? posts.map((post) => post.products.length).reduce((previousValue, currentValue) => previousValue + currentValue)
+      : 0
 
   const onReset = () => {
-    formMethods.reset()
-    router.replace(pathname)
+    router.replace(pathname, { scroll: false })
+  }
+
+  const onChange = (filterName: string, value: string) => {
+    const newSearchParams = createQueryString(filterName, value)
+    router.replace(`${pathname}?${newSearchParams}`, { scroll: false })
   }
 
   return (
@@ -140,57 +143,26 @@ const AgreementPage = ({ agreement }: { agreement: Agreement }) => {
       <VStack className="main-wrapper--large spacing-bottom--xlarge hide-print">
         <TopBar agreement={agreement} />
 
-        <FormProvider {...formMethods}>
-          <CompareMenu />
+        <CompareMenu />
 
-          <VStack gap={{ xs: '4', md: '8' }} paddingBlock={'12'}>
-            <Heading level="2" size={'medium'}>
-              {totalProducts} hjelpemidler i delkontrakter
-            </Heading>
+        <VStack gap={{ xs: '4', md: '8' }} paddingBlock={'12'}>
+          <Heading level="2" size={'medium'}>
+            {totalProducts} hjelpemidler i delkontrakter
+          </Heading>
 
-            <HStack justify="space-between" align="center" gap="2" className="spacing-bottom--medium">
-              {showSidebar && <FilterForm onSubmit={onSubmit} ref={searchFormRef} filters={filters} />}
-              {!showSidebar && (
-                <HStack gap="2">
-                  <Button
-                    variant="secondary-neutral"
-                    className="button-with-thin-border"
-                    onClick={() => setMobileOverlayOpen(true)}
-                    icon={<FilterIcon aria-hidden />}
-                  >
-                    Filter
-                  </Button>
-                  <Show below="sm">
-                    <Button
-                      variant="secondary"
-                      onClick={() => {
-                        window.print()
-                      }}
-                      icon={<FilePdfIcon aria-hidden fontSize="1.5rem" />}
-                      iconPosition={'right'}
-                    >
-                      Skriv ut
-                    </Button>
-                  </Show>
-                </HStack>
-              )}
-
-              <MobileOverlayModal
-                body={<FilterForm onSubmit={onSubmit} ref={searchFormRef} filters={filters} />}
-                onReset={onReset}
-              />
-
-              <HStack gap="4">
+          <HStack justify="space-between" align="center" gap="2" className="spacing-bottom--medium">
+            {showSidebar && <FilterForm filters={filters} onChange={onChange} />}
+            {!showSidebar && (
+              <HStack gap="2">
                 <Button
-                  icon={<ImageIcon aria-hidden />}
-                  iconPosition={'right'}
-                  variant={'secondary'}
-                  onClick={() => handleShowHidePics()}
+                  variant="secondary-neutral"
+                  className="button-with-thin-border"
+                  onClick={() => setMobileOverlayOpen(true)}
+                  icon={<FilterIcon aria-hidden />}
                 >
-                  Vis/skjul bilder
+                  Filter
                 </Button>
-
-                <Show above="sm">
+                <Show below="sm">
                   <Button
                     variant="secondary"
                     onClick={() => {
@@ -203,25 +175,51 @@ const AgreementPage = ({ agreement }: { agreement: Agreement }) => {
                   </Button>
                 </Show>
               </HStack>
+            )}
+
+            <MobileOverlayModal body={<FilterForm filters={filters} onChange={onChange} />} onReset={onReset} />
+
+            <HStack gap="4">
+              <Button
+                icon={<ImageIcon aria-hidden />}
+                iconPosition={'right'}
+                variant={'secondary'}
+                onClick={() => handleShowHidePics()}
+              >
+                Vis/skjul bilder
+              </Button>
+
+              <Show above="sm">
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    window.print()
+                  }}
+                  icon={<FilePdfIcon aria-hidden fontSize="1.5rem" />}
+                  iconPosition={'right'}
+                >
+                  Skriv ut
+                </Button>
+              </Show>
             </HStack>
+          </HStack>
 
-            {avtalerMedIsoGruppering.includes(agreement.id) ? (
-              <PostsListIsoGroups posts={posts} postLoading={postsIsLoading} postError={postError} />
-            ) : (
-              <PostsList posts={posts} postLoading={postsIsLoading} postError={postError} />
-            )}
+          {avtalerMedIsoGruppering.includes(agreement.id) ? (
+            <PostsListIsoGroups posts={posts} postLoading={postsIsLoading} postError={postError} />
+          ) : (
+            <PostsList posts={posts} postLoading={postsIsLoading} postError={postError} />
+          )}
 
-            {postError && (
-              <Alert variant="error" title="Error med lasting av produkter">
-                Det har skjedd en feil ved innhenting av produkter. Vennligst prøv igjen senere.
-              </Alert>
-            )}
+          {postError && (
+            <Alert variant="error" title="Error med lasting av produkter">
+              Det har skjedd en feil ved innhenting av produkter. Vennligst prøv igjen senere.
+            </Alert>
+          )}
 
-            {!postError && posts.length === 0 && (
-              <Alert variant="info">Obs! Fant ingen hjelpemiddel. Har du sjekket filtrene dine?</Alert>
-            )}
-          </VStack>
-        </FormProvider>
+          {!postError && posts.length === 0 && (
+            <Alert variant="info">Obs! Fant ingen hjelpemiddel. Har du sjekket filtrene dine?</Alert>
+          )}
+        </VStack>
       </VStack>
     </>
   )
