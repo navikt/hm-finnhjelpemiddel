314c2e7f55914d96e68f7f70b4fb226e130613e6
Atle Nordland <atle.nordland@nav.no>
2025-10-10 15:32:42 +0200

    wip: ny alternativredigering

--- src/app/gjenbruksprodukter/rediger/EditableAlternativeCard.tsx
+++ src/app/gjenbruksprodukter/rediger/EditableAlternativeCard.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import { Stack } from '@navikt/ds-react'
-import styles from '@/app/gjenbruksprodukter/AlternativeProducts.module.scss'
+import { BodyShort, Box, Button, HGrid } from '@navikt/ds-react'
+import styles from './EditableAlternativeCard.module.scss'
 import { AlternativeProduct } from '@/app/gjenbruksprodukter/alternative-util'
+import ProductImage from '@/components/ProductImage'
 
 export const EditableAlternativeCard = ({ alternativeProduct }: { alternativeProduct: AlternativeProduct }) => {
   return (
-    <Stack align={'start'} direction={'column'} className={styles.alternativeProductContainer}>
-      {alternativeProduct.hmsArtNr}
-    </Stack>
+    <HGrid
+      columns={'.5fr 3fr .8fr .8fr .8fr'}
+      gap={'2'}
+      padding={'4'}
+      align={'center'}
+      className={styles.container}
+      width={'10px'}
+    >
+      <BodyShort>
+        <Box paddingInline="2" paddingBlock="2" className={styles.imageWrapper}>
+          <ProductImage src={alternativeProduct.imageUri} productTitle={'produktbilde'}></ProductImage>
+        </Box>
+      </BodyShort>
+      <BodyShort>{alternativeProduct.variantTitle}</BodyShort>
+      <BodyShort>HMS: {alternativeProduct.hmsArtNr}</BodyShort>
+      <BodyShort>Rangering: {alternativeProduct.highestRank}</BodyShort>
+      <Button variant={'secondary'} style={{ width: 'fit-content' }}>
+        Slett
+      </Button>
+    </HGrid>
   )
 }
