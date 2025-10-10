8093df70ff4f5d5eead8c785de24f027e8bfa8e0
marte pedersen <marte.pedersen@nav.no>
2025-10-10 18:56:46 +0200

    x-knapp for å lukke popup

--- src/app/SkyraSurvey.tsx
+++ src/app/SkyraSurvey.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { Box, Button, Popover } from '@navikt/ds-react'
+import { Box, Button, Popover, VStack } from '@navikt/ds-react'
 import { useRef, useState } from 'react'
 import styles from './SkyraSurvey.module.scss'
+import { XMarkIcon } from '@navikt/aksel-icons'
 
 export const SkyraSurvey = ({ buttonText, skyraSlug }: { buttonText: string; skyraSlug: string }) => {
   const buttonRef = useRef<HTMLButtonElement>(null)
@@ -20,9 +21,19 @@ export const SkyraSurvey = ({ buttonText, skyraSlug }: { buttonText: string; sky
       </Button>
 
       <Popover placement="bottom" open={openState} onClose={() => setOpenState(false)} anchorEl={buttonRef.current}>
-        <Popover.Content style={{ width: '360px' }}>
-          {/* @ts-expect-error Ikke typet */}
-          <skyra-survey slug={skyraSlug} />
+        <Popover.Content style={{ width: '360px', paddingTop: '10px' }}>
+          <VStack gap={'0'}>
+            <Button
+              className={styles.closeButton}
+              variant={'tertiary'}
+              size={'xsmall'}
+              icon={<XMarkIcon aria-hidden />}
+              title={'Lukk'}
+              onClick={() => setOpenState(false)}
+            />
+            {/* @ts-expect-error Ikke typet */}
+            <skyra-survey slug={skyraSlug} />
+          </VStack>
         </Popover.Content>
       </Popover>
     </Box>
