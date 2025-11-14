d3b41a33703a6527ec73af475f40f6433c7728f3
marte pedersen <marte.pedersen@nav.no>
2025-11-14 10:29:28 +0100

    wip på bevegelse

---
+++ src/app/kategori/bevegelse/CategoryCard.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { LinkCard, VStack } from '@navikt/ds-react'
+import NextLink from 'next/link'
+
+type Props = {
+  icon: React.ReactNode
+  title: string
+  link: string
+  description: string
+}
+
+export const CategoryCard = ({ icon, title, link, description }: Props) => {
+  return (
+    <LinkCard arrow={false}>
+      <VStack justify="center" height="100%" asChild>
+        <LinkCard.Icon>{icon}</LinkCard.Icon>
+      </VStack>
+      <LinkCard.Title>
+        <LinkCard.Anchor asChild>
+          <NextLink href={link}>{title}</NextLink>
+        </LinkCard.Anchor>
+      </LinkCard.Title>
+      <LinkCard.Description>{description}</LinkCard.Description>
+    </LinkCard>
+  )
+}
---
+++ src/app/kategori/bevegelse/page.tsx
@@ -0,0 +1,42 @@
+import { Bleed, BodyLong, Heading, HGrid, VStack } from '@navikt/ds-react'
+import { CategoryCard } from '@/app/kategori/bevegelse/CategoryCard'
+import { WheelchairIcon } from '@navikt/aksel-icons'
+
+export default async function Page() {
+  const icon1 = <WheelchairIcon fontSize={'5rem'} />
+  const title1 = 'Manuelle rullestoler'
+  const link1 = '/manuelle-rullestoler'
+  const description1 =
+    'Rullestoler som manøvreres ved at den som sitter i stolen, bruker begge hender på dekkene eller på hjulenes drivringer. Omfatter f.eks. framhjulsdrevne og bakhjulsdrevne rullestoler.'
+
+  return (
+    <VStack
+      gap={'14'}
+      paddingBlock={'16'}
+      paddingInline={'4'}
+      marginInline={'auto'}
+      marginBlock={'0'}
+      maxWidth={'1440px'}
+    >
+      <VStack gap="4">
+        <Heading level="1" size="large">
+          Bevegelse
+        </Heading>
+        <BodyLong style={{ maxWidth: '735px' }}>
+          Her finner du hjelpemidler for aktiviteter og deltakelse relatert til personforflytning og transport.
+        </BodyLong>
+      </VStack>
+
+      <Bleed marginInline="full" reflectivePadding style={{ backgroundColor: '#F5F9FF' }}>
+        <div>
+          <HGrid gap={'2'} columns={'repeat(2, 600px)'}>
+            <CategoryCard icon={icon1} title={title1} link={link1} description={description1} />
+            <CategoryCard icon={icon1} title={title1} link={link1} description={description1} />
+            <CategoryCard icon={icon1} title={title1} link={link1} description={description1} />
+            <CategoryCard icon={icon1} title={title1} link={link1} description={description1} />
+          </HGrid>
+        </div>
+      </Bleed>
+    </VStack>
+  )
+}
