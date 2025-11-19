1db15560841b7c8b808e020756e2dfa5b4977b9a
Aasmund Nordstoga <anostoga@gmail.com>
2025-11-19 12:28:20 +0100

    Vekter ned deler litt meir

--- src/utils/api-util.ts
+++ src/utils/api-util.ts
@@ -209,7 +209,7 @@ export const makeSearchTermQuery = ({
       },
     },
     //Ganges med 1 betyr samme boost. Ganges med et mindre tall betyr lavere boost og kommer lenger ned. Om den settes til 0 forsvinner den helt fordi alt som ganges med 0 er 0
-    negative_boost: 0.4,
+    negative_boost: 0.3,
   }
 
   const negativeBoostSparePartsAndAccessories = {
