13239c9b17614f0779fa305d56917a51acb70fe6
marte pedersen <marte.pedersen@nav.no>
2025-12-02 16:39:42 +0100

    refaktorering og styling

--- src/app/kategori/MinMaxFilter.tsx
+++ src/app/kategori/MinMaxFilter.tsx
@@ -1,6 +1,6 @@
-import { ActionMenu, Button, TextField } from '@navikt/ds-react'
+import { ActionMenu, Button, HStack, TextField } from '@navikt/ds-react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { ChevronDownIcon, ChevronUpIcon, TrashIcon } from '@navikt/aksel-icons'
 import styles from './MinMaxFilter.module.scss'
 import useQueryString from '@/utils/search-params-util'
@@ -31,10 +31,12 @@ export const MinMaxFilter = ({ filterMenu }: Props) => {
 
   const { options, name } = filterMenu
   const [menuOpen, setMenuOpen] = useState(false)
+  const menuTriggerRef = useRef<HTMLButtonElement>(null)
 
   const filterLabel = name
 
-  const hasInputValue = searchParams.get(options.min.key) || searchParams.get(options.max.key)
+  const hasSearchParam = searchParams.get(options.min.key) || searchParams.get(options.max.key)
+  const [inputValue, setInputValue] = useState(hasSearchParam ?? '')
 
   const onChange = (value: string) => {
     const newSearchParams = createQueryStringForMinMax(
@@ -44,38 +46,52 @@ export const MinMaxFilter = ({ filterMenu }: Props) => {
     router.replace(`${pathname}?${newSearchParams}`, { scroll: false })
   }
 
+  const setValue = () => {
+    menuTriggerRef?.current?.click()
+    onChange(inputValue)
+  }
+
   const onReset = () => {
     onChange('')
   }
 
   return (
     <ActionMenu onOpenChange={(open) => setMenuOpen(open)}>
-      <ActionMenu.Trigger>
+      <ActionMenu.Trigger ref={menuTriggerRef}>
         <Button
           variant={'secondary'}
           size={'small'}
           icon={menuOpen ? <ChevronUpIcon aria-hidden /> : <ChevronDownIcon aria-hidden />}
           iconPosition={'right'}
-          className={hasInputValue ? styles.filterButtonActive : styles.filterButton}
+          className={hasSearchParam ? styles.filterButtonActive : styles.filterButton}
         >
           {filterLabel}
         </Button>
       </ActionMenu.Trigger>
       <ActionMenu.Content className={styles.filterMenu}>
-        {hasInputValue && (
+        {hasSearchParam && (
           <ActionMenu.Item variant={'danger'} icon={<TrashIcon />} onSelect={onReset}>
             Fjern filter
           </ActionMenu.Item>
         )}
-        <TextField
-          label={filterLabel}
-          hideLabel
-          inputMode={'numeric'}
-          size={'small'}
-          min={0}
-          defaultValue={hasInputValue ?? undefined}
-          onChange={(event) => onChange(event.currentTarget.value)}
-        />
+        <HStack gap={'2'}>
+          <TextField
+            label={filterLabel}
+            hideLabel
+            inputMode={'numeric'}
+            size={'small'}
+            min={0}
+            defaultValue={inputValue}
+            onChange={(event) => setInputValue(event.currentTarget.value)}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') setValue()
+            }}
+            style={{ width: '80px' }}
+          />
+          <Button variant={'tertiary'} size={'xsmall'} onClick={setValue}>
+            Bruk
+          </Button>
+        </HStack>
       </ActionMenu.Content>
     </ActionMenu>
   )
