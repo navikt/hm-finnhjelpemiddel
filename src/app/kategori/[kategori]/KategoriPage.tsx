f53a077ec8d276be6a438c9bf630a9881d31cdcc
Aasmund Nordstoga <anostoga@gmail.com>
2026-01-08 14:10:53 +0100

    Remove useMemo from KategoriPage for simplification

--- src/app/kategori/[kategori]/KategoriPage.tsx
+++ src/app/kategori/[kategori]/KategoriPage.tsx
@@ -59,11 +59,6 @@ export const KategoriPage = ({ kategori }: Props) => {
   }
   const searchData = mapSearchParamsKategori(searchParams)
 
-  const searchParamsKey = useMemo(() => {
-    const params = new URLSearchParams(searchParams)
-    return params.toString()
-  }, [searchParams])
-
   const currentKategori = kategorier[kategori]
 
   const {
@@ -106,7 +101,7 @@ export const KategoriPage = ({ kategori }: Props) => {
       router.replace(`${pathname}?${searchQueryString}`, { scroll: false })
       setPage(nextPage)
     }
-  }, [productsData, page, setPage, pathname, router, searchParamsKey])
+  }, [productsData, page, setPage, pathname, router, searchParams])
 
   const products = productsData?.map((d) => d.products).flat()
   const isos = productsData?.at(-1)?.iso.map((iso) => ({ key: iso.code, label: iso.name })) ?? []
