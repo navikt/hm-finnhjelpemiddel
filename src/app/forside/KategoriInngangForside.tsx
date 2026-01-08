291195ac73c887674cb444f4fd442d8d8bbced75
Aasmund Nordstoga <anostoga@gmail.com>
2026-01-08 09:22:28 +0100

    Fjerner tekst om at tester nye ting etter diskusjon i teamet.

--- src/app/forside/KategoriInngangForside.tsx
+++ src/app/forside/KategoriInngangForside.tsx
@@ -19,13 +19,10 @@ export const KategoriInngangForside = () => {
   })
 
   return (
-    <VStack gap={'2'} paddingInline={{ lg: '6' }}>
+    <VStack gap={'4'} paddingInline={{ lg: '6' }}>
       <Heading level={'2'} size={'large'}>
         Kategori-inngang (beta)
       </Heading>
-      <BodyLong size={'large'} style={{ maxWidth: '600px' }}>
-        Vi tester en ny måte å finne hjelpemidler på.
-      </BodyLong>
       <HGrid gap={'4'} columns={{ xs: 1, md: 2 }} paddingBlock={'2 0'} maxWidth={'700px'}>
         <CategoryCard title={'Bevegelse'} link={'/kategori/Bevegelse'} icon={BevegelseIkon()} />
       </HGrid>
