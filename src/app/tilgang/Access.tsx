b0bdc3f7af313ec0e9b89b6b585ea6b667c8e5f0
marte pedersen <marte.pedersen@nav.no>
2026-01-29 11:37:50 +0100

    Fiks på tekst

--- src/app/tilgang/Access.tsx
+++ src/app/tilgang/Access.tsx
@@ -14,7 +14,6 @@ export const Access = () => {
       <Heading level="1" size="large" className="spacing-bottom--medium">
         Beklager, brukeren din har ikke tilgang til denne siden.
       </Heading>
-      <BodyShort>Ta kontakt med Team Mime/Digihot</BodyShort>
       <Link href="/" style={{ width: 'fit-content' }}>
         Gå til forsiden
       </Link>
