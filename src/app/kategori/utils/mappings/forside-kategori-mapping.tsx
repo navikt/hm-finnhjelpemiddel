b8c31bf82f602eb3a7a42dd92960eeb687585eaa
Arshad A. Kamran <arshad.ali.kamran@nav.no>
2026-02-02 15:15:42 +0100

    ny kategori sport of aktivitet

--- src/app/kategori/utils/mappings/forside-kategori-mapping.tsx
+++ src/app/kategori/utils/mappings/forside-kategori-mapping.tsx
@@ -1,9 +1,10 @@
 import { JSX } from 'react'
 import { BevegelseIkon } from '@/app/kategori/ikoner/BevegelseIkon'
 import { HygieneIkon } from '@/app/kategori/ikoner/HygieneIkon'
-import { HjeomOgBoligIkon } from '@/app/kategori/ikoner/HjeomOgBoligIkon'
+import { HjemOgBoligIkon } from '@/app/kategori/ikoner/HjemOgBoligIkon'
+import { SportOgAktivitetIkon } from '@/app/kategori/ikoner/SportOgAktivitetIkon'
 
-export type FrontPageCategoryTitles = 'Bevegelse' | 'Hygiene' | 'Hjem og bolig'
+export type FrontPageCategoryTitles = 'Bevegelse' | 'Hygiene' | 'Hjem og bolig' | 'Sport og aktivitet'
 export type TopLevelCategoryTitles = FrontPageCategoryTitles | 'Alle'
 
 type TopLevelCategories = {
@@ -36,20 +37,29 @@ const hygiene: TopLevelCategory = {
 const hjemOgBolig: TopLevelCategory = {
   title: 'Hjem og bolig',
   description: '',
-  icon: <HjeomOgBoligIkon />,
+  icon: <HjemOgBoligIkon />,
+}
+
+const sportOgAktivitet: TopLevelCategory = {
+  title: 'Sport og aktivitet',
+  description: '',
+  icon: <SportOgAktivitetIkon />,
 }
 
 export const topLevelcategories: TopLevelCategories = {
   Bevegelse: bevegelse,
   Hygiene: hygiene,
   'Hjem og bolig': hjemOgBolig,
+  'Sport og aktivitet': sportOgAktivitet,
   Alle: alle,
 }
 export const frontPageCategories: FrontPageCategories = {
   Bevegelse: bevegelse,
   'Hjem og bolig': hjemOgBolig,
   Hygiene: hygiene,
+  'Sport og aktivitet': sportOgAktivitet,
 }
 
-export const topLevelCategoryTitles: TopLevelCategoryTitles[] = ['Bevegelse', 'Hygiene', 'Hjem og bolig', 'Alle']
-export const frontPageTitles: FrontPageCategoryTitles[] = ['Bevegelse', 'Hjem og bolig', 'Hygiene']
+
+export const topLevelCategoryTitles: TopLevelCategoryTitles[] = ['Bevegelse', 'Hygiene', 'Hjem og bolig', 'Sport og aktivitet','Alle']
+export const frontPageTitles: FrontPageCategoryTitles[] = ['Bevegelse', 'Hjem og bolig', 'Hygiene', 'Sport og aktivitet']
