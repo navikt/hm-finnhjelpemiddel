cf459014d2b1327420fbabfae40b0104ae166ca2
Arshad A. Kamran <arshad.ali.kamran@nav.no>
2026-02-11 12:55:23 +0100

    ikke vise siden med hjelpemidler for avtalen

--- src/app/rammeavtale/hjelpemidler/[agreementId]/page.tsx
+++ src/app/rammeavtale/hjelpemidler/[agreementId]/page.tsx
@@ -3,13 +3,14 @@ import { getAgreement } from '@/utils/api-util'
 import { Metadata } from 'next'
 import { Suspense } from 'react'
 import AgreementPage from './AgreementPage'
+import { NotFound } from '@/app/[...not-found]/NotFound'
 
 type Props = {
   params: Promise<{ agreementId: string }>
 }
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
-  const params = await props.params;
+  const params = await props.params
   const agreementId = params.agreementId
   const agreementResponse = await getAgreement(agreementId)
   const agreement = mapAgreementFromDoc(agreementResponse)
@@ -23,11 +24,8 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
 }
 
 export default async function Page(props: Props) {
-  const params = await props.params;
+  const params = await props.params
   const agreement = mapAgreementFromDoc(await getAgreement(params.agreementId))
-  return (
-    <Suspense>
-      <AgreementPage agreement={agreement} />
-    </Suspense>
-  )
+  const activeAgreement = agreement.published <= new Date() && agreement.expired >= new Date()
+  return <Suspense>{activeAgreement ? <AgreementPage agreement={agreement} /> : <NotFound />}</Suspense>
 }
