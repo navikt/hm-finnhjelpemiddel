b81394e77331567a4961c73f9df8ea900222867a
Atle Nordland <atle.nordland@nav.no>
2026-02-25 10:35:51 +0100

    fjern bakgrunnsfarge

--- src/app/kategori/KategoriOversikt.tsx
+++ src/app/kategori/KategoriOversikt.tsx
@@ -1,4 +1,4 @@
-import { Bleed, HGrid } from '@navikt/ds-react'
+import { HGrid } from '@navikt/ds-react'
 import { CategoryCard } from '@/app/kategori/CategoryCard'
 import { KategoriPageLayout } from '@/app/kategori/KategoriPageLayout'
 import { CategoryDTO } from '@/app/kategori/admin/category-admin-util'
@@ -7,21 +7,19 @@ export const KategoriOversikt = ({ category }: { category: CategoryDTO }) => {
   return (
     <KategoriPageLayout title={category.title} description={category.data.description}>
       {category.subCategories?.length && (
-        <Bleed style={{ backgroundColor: '#F5F9FF' }} reflectivePadding marginInline={'full'}>
-          <HGrid gap={'space-16'} columns={{ xs: 1, md: 2 }} paddingBlock={'space-48 space-96'}>
-            {category.subCategories
-              .sort((a, b) => a.title.localeCompare(b.title))
-              .map((subCategory) => (
-                <CategoryCard
-                  icon={subCategory.icon}
-                  title={subCategory.title}
-                  link={subCategory.title}
-                  key={subCategory.title}
-                  showSubCategoryIcons={category.data.showSubCategoryIcons}
-                />
-              ))}
-          </HGrid>
-        </Bleed>
+        <HGrid gap={'space-16'} columns={{ xs: 1, md: 2 }} paddingBlock={'space-48 space-96'}>
+          {category.subCategories
+            .sort((a, b) => a.title.localeCompare(b.title))
+            .map((subCategory) => (
+              <CategoryCard
+                icon={subCategory.icon}
+                title={subCategory.title}
+                link={subCategory.title}
+                key={subCategory.title}
+                showSubCategoryIcons={category.data.showSubCategoryIcons}
+              />
+            ))}
+        </HGrid>
       )}
     </KategoriPageLayout>
   )
