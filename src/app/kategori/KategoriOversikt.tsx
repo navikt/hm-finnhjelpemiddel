919c78a108aaaa1e875c48cff31c5b47e143ed4e
Atle Nordland <atle.nordland@nav.no>
2026-03-09 10:47:46 +0100

    ux-signal på kategori-side

--- src/app/kategori/KategoriOversikt.tsx
+++ src/app/kategori/KategoriOversikt.tsx
@@ -2,10 +2,13 @@ import { HGrid } from '@navikt/ds-react'
 import { CategoryCard } from '@/app/kategori/CategoryCard'
 import { KategoriPageLayout } from '@/app/kategori/KategoriPageLayout'
 import { CategoryDTO } from '@/app/kategori/admin/category-admin-util'
+import Script from 'next/script'
 
 export const KategoriOversikt = ({ category }: { category: CategoryDTO }) => {
   return (
     <KategoriPageLayout title={category.title} description={category.data.description}>
+      <div data-uxsignals-embed="panel-p9bmli1xkl" style={{ maxWidth: '620px' }} />
+      <Script src="https://widget.uxsignals.com/embed.js"></Script>
       {category.subCategories?.length && (
         <HGrid gap={'space-16'} columns={{ xs: 1, md: 2 }} paddingBlock={'space-0 space-96'}>
           {category.subCategories
