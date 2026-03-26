5bf53d785423bfcdc1673bda659ee414fb856d93
Arshad A. Kamran <arshad.ali.kamran@nav.no>
2026-03-26 09:12:23 +0100

    ny tekst - rettigheter og ansvar

--- src/app/rettigheter-og-ansvar/page.tsx
+++ src/app/rettigheter-og-ansvar/page.tsx
@@ -14,6 +14,9 @@ function RightsResponsibilities() {
           <Heading level="1" size="medium" spacing>
             Rettigheter og ansvar
           </Heading>
+          <Heading level="2" size="medium" spacing>
+            Finnhjelpemiddel: Eierskap og ansvar
+          </Heading>
           <BodyLong spacing>
             Innholdet i FinnHjelpemiddel er opphavsrettslig beskyttet. Det må ikke reproduseres, endres, overføres eller
             publiseres helt eller delvis uten samtykke fra Nav.
@@ -25,18 +28,38 @@ function RightsResponsibilities() {
           <ul>
             <li>Opplysninger i FinnHjelpemiddel kan bli endret eller oppdatert uten varsel.</li>
             <li>Feil kan forekomme.</li>
-            <li>Nav påtar seg ikke ansvaret for innholdet i produktinformasjonen.</li>
+            <li>Nav påtar seg ikke ansvaret for innholdet i produktinformasjonen (se mer informasjon under).</li>
             <li>Bruk av opplysningene skjer på eget ansvar.</li>
-            <li>Informasjonen på FinnHjelpemiddel er basert på informasjon gitt av leverandører av hjelpemidler.</li>
-            <li>
-              Nav fraskriver seg ansvar for direkte eller indirekte tap som er en følge av bruk av opplysningene gitt i
-              FinnHjelpemiddel, uavhengig av om tapet skyldes feil, uklare opplysninger, virus eller har andre årsaker.
-            </li>
-            <li>
-              Nav støtter ikke andre nettsteder som lenkes til fra FinnHjelpemiddel og påtar seg ikke ansvar for
-              innholdet på disse nettstedene.
-            </li>
+            <li>Informasjonen på FinnHjelpemiddel er basert på informasjon gitt av leverandørene.</li>
           </ul>
+          <br />
+          <Heading level="2" size="medium" spacing>
+            Ansvarsforhold
+          </Heading>
+          <BodyLong spacing>
+            Nav fraskriver seg ansvar for direkte eller indirekte tap som er en følge av bruk av opplysningene fra
+            FinnHjelpemiddel, uavhengig av om tapet skyldes feil, uklare opplysninger, virus eller har andre årsaker.
+            For hjelpemidler på avtale med Nav gjennomgås det en form for kvalitetskontroll i forbindelse med
+            anskaffelsesprosessen.
+          </BodyLong>
+          <BodyLong spacing>
+            Nav støtter ikke andre nettsteder som lenkes til fra FinnHjelpemiddel og påtar seg ikke ansvar for innholdet
+            på disse nettstedene. Informasjonen du finner om hjelpemidlene, tilbehøret og reservedelene leveres av
+            leverandører av hjelpemidler.
+          </BodyLong>
+          <Heading level="2" size="medium" spacing>
+            Klassifisering etter internasjonal standard
+          </Heading>
+          <BodyLong spacing>
+            Alle hjelpemidlene i FinnHjelpemiddel klassifiseres ut ifra internasjonal standard for klassifisering av
+            hjelpemidler, ISO 9999. Nav administrerer og validerer klassifiseringen og tildelingen av HMS nr. der det
+            finnes.
+          </BodyLong>
+          <BodyLong spacing>
+            Med utgangspunkt i ISO 9999s definisjon av et hjelpemiddel, begrenser FinnHjelpemiddel seg ikke kun til
+            hjelpemidler som kan bevilges etter en bestemt lovgivning, eller hjelpemidler som er omfattet av et bestemt
+            direktivområde.
+          </BodyLong>
         </article>
       </div>
     </div>
