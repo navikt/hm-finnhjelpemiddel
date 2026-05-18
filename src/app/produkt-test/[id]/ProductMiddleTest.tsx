1c6314346203842da82ea7b975658c919ff447a3
Atle Nordland <atle.nordland@nav.no>
2026-05-18 14:35:16 +0200

    poc teknisk data-gruppering

---
+++ src/app/produkt-test/[id]/ProductMiddleTest.tsx
@@ -0,0 +1,198 @@
+'use client'
+
+import { Box, Heading, HGrid, Link, Table, VStack } from '@navikt/ds-react'
+import { AgreementInfo, Product, ProductVariant } from '@/utils/product-util'
+import { ProductInformation } from '@/app/produkt/[id]/ProductInformation'
+import NextLink from 'next/link'
+import styles from './ProductMiddleTest.module.scss'
+
+import { useMemo } from 'react'
+import { useFeatureFlags } from '@/hooks/useFeatureFlag'
+import { WorksWith } from '@/app/produkt/[id]/WorksWith'
+import { toValueAndUnit } from '@/utils/string-util'
+
+const WORKS_WITH_CONFIG = {
+  featureFlag: 'finnhjelpemiddel.vis-virker-sammen-med-products',
+  agreementIds: new Set(['7ef2ab32-34bd-4eec-92a8-2b5c47b77c78', '47105bc7-10a2-48fc-9ff2-95d6e7bb6b96']),
+  agreementTitles: new Set(['Varslingshjelpemidler', 'Hørselshjelpemidler']),
+}
+
+const groupTechData = (variants: ProductVariant[]): Map<string, string[]> => {
+  const map = new Map<string, string[]>()
+
+  const allDataLabels = new Map(
+    variants.flatMap((variant) => {
+      return Object.entries(variant.techData).map(([key, value]) => {
+        return [key, { key: key, unit: value.unit }]
+      })
+    })
+  )
+
+  const målOgVekt: string[] = []
+  const seteting: string[] = []
+  const armleneting: string[] = []
+
+  const diverse: string[] = []
+
+  allDataLabels.forEach(function (label, key) {
+    /*if (['cm', 'tommer', 'kg'].includes(label.unit.toLowerCase())) {
+      målOgVekt.push(key)
+    }
+
+     */
+    console.log(key)
+
+    if (/sete/i.test(key)) {
+      console.log('aaaa')
+      seteting.push(key)
+    } else if (/armlene/i.test(key)) {
+      armleneting.push(key)
+    } else if (['cm', 'tommer', 'kg'].includes(label.unit.toLowerCase())) {
+      målOgVekt.push(key)
+    } else {
+      diverse.push(key)
+    }
+  })
+
+  map.set('Mål og vekt', målOgVekt)
+  map.set('Diverse', diverse)
+  map.set('Seteting', seteting)
+  map.set('Armleneting', armleneting)
+
+  return map
+}
+
+const TechDataTable = ({
+  title,
+  dataKeys,
+  variants,
+}: {
+  title: string
+  dataKeys: string[]
+  variants: ProductVariant[]
+}) => {
+  const dataRows: { [key: string]: string } = Object.assign(
+    {},
+    ...dataKeys.map((key) => ({
+      [key]: toValueAndUnit(variants[0].techData[key].value, variants[0].techData[key].unit),
+    }))
+  )
+
+  if (Object.keys(dataRows).length === 0) {
+    return <></>
+  }
+  return (
+    <VStack>
+      <Heading level="2" size="medium">
+        {title}
+      </Heading>
+      <Box paddingBlock="space-16">
+        <Table className={styles.commonAttributes}>
+          <Table.Body>
+            {Object.entries(dataRows).map(([key, row]) => {
+              return (
+                <Table.Row key={key}>
+                  <Table.HeaderCell>{key}</Table.HeaderCell>
+                  <Table.DataCell>{row}</Table.DataCell>
+                </Table.Row>
+              )
+            })}
+          </Table.Body>
+        </Table>
+      </Box>
+    </VStack>
+  )
+}
+
+const ProductMiddleTest = ({ product }: { product: Product }) => {
+  const worksWithSeriesIds = product.attributes.worksWith?.seriesIds
+
+  const featureFlags = useFeatureFlags()
+
+  const worksWithFeatureFlag: boolean = featureFlags.isEnabled(WORKS_WITH_CONFIG.featureFlag) ?? false
+
+  const shouldShowSection = useMemo(() => {
+    return product.agreements.some(
+      (agreement) =>
+        WORKS_WITH_CONFIG.agreementIds.has(agreement.id) || WORKS_WITH_CONFIG.agreementTitles.has(agreement.title)
+    )
+  }, [product.agreements])
+
+  const worksWithShowConstrain = worksWithFeatureFlag && shouldShowSection
+
+  const groupedTechData = groupTechData(product.variants)
+
+  console.log(groupedTechData)
+
+  return (
+    <HGrid
+      gap={'space-80 space-32'}
+      columns={{ sm: 1, md: 2 }}
+      className={styles.middleContainer}
+      paddingBlock={'space-24 space-0'}
+    >
+      <div style={{ gridArea: 'box1' }}>
+        <ProductInformation product={product} />
+      </div>
+      <VStack gap={'space-24'} style={{ gridArea: 'box2' }}>
+        {product.agreements.length > 0 && <OtherProductsOnPost agreements={product.agreements} />}
+
+        {worksWithShowConstrain && <WorksWith worksWithSeriesIds={worksWithSeriesIds} />}
+      </VStack>
+      <div style={{ gridArea: 'box3' }}>
+        <>
+          <TechDataTable
+            title={'Seteting'}
+            dataKeys={groupedTechData.get('Seteting')?.sort() ?? []}
+            variants={product.variants}
+          />
+          <TechDataTable
+            title={'Armeleneting'}
+            dataKeys={groupedTechData.get('Armleneting')?.sort() ?? []}
+            variants={product.variants}
+          />
+          <TechDataTable
+            title={'Mål og vekt'}
+            dataKeys={groupedTechData.get('Mål og vekt')?.sort() ?? []}
+            variants={product.variants}
+          />
+          <TechDataTable
+            title={'Diverse'}
+            dataKeys={groupedTechData.get('Diverse')?.sort() ?? []}
+            variants={product.variants}
+          />
+        </>
+      </div>
+    </HGrid>
+  )
+}
+
+const showOtherProductsOnAgreement = ({ agreement, index }: { agreement: AgreementInfo; index: number }) => {
+  return (
+    <VStack gap={'space-8'} paddingBlock={'space-8 space-16'} key={index}>
+      <Link as={NextLink} href={`/rammeavtale/hjelpemidler/${agreement.id}#${agreement.refNr}`}>
+        {agreement.postTitle}
+      </Link>
+    </VStack>
+  )
+}
+
+const OtherProductsOnPost = ({ agreements }: { agreements: AgreementInfo[] }) => {
+  const sortedAgreements = [...agreements].sort((a, b) => {
+    return b.postNr !== a.postNr ? a.postNr - b.postNr : !a.refNr ? -1 : !b.refNr ? 1 : b.refNr.localeCompare(a.refNr)
+  })
+
+  return (
+    <VStack gap={'space-8'} paddingInline={'space-8 space-0'}>
+      <Heading size={'medium'} level={'2'}>
+        Andre hjelpemidler på delkontrakt:
+      </Heading>
+      {sortedAgreements.length > 0 &&
+        sortedAgreements.map((agreement, index) => {
+          return showOtherProductsOnAgreement({ agreement: agreement, index })
+        })}
+    </VStack>
+  )
+}
+
+export default ProductMiddleTest
---
+++ src/app/produkt-test/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { fetchProductsWithVariants, getProductWithVariants } from '@/utils/api-util'
+import { mapProductFromSeriesId } from '@/utils/product-util'
+import { Metadata } from 'next'
+import ProductTop from '@/app/produkt/[id]/ProductTop'
+import { VariantTable } from '@/app/produkt/[id]/variantTable/VariantTable'
+import AccessoryOrSparePartPage from '@/app/produkt/AccessoryOrSparePartPage'
+import { ProductPageLayout } from '@/app/produkt/ProductPageLayout'
+import ProductMiddleTest from '@/app/produkt-test/[id]/ProductMiddleTest'
+
+type Props = {
+  params: Promise<{ id: string }>
+}
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const params = await props.params
+  const product = mapProductFromSeriesId(await getProductWithVariants(params.id))
+
+  return {
+    title: product.title,
+    description: 'Produktside for ' + product.title,
+  }
+}
+
+export default async function ProduktPage(props: Props) {
+  const params = await props.params
+
+  const product = mapProductFromSeriesId(await getProductWithVariants(params.id))
+  const isAccessoryOrSparePart = !product.main
+  const matchingSeriesIds = product.attributes.compatibleWith?.seriesIds
+
+  const matchingProducts = (matchingSeriesIds && (await fetchProductsWithVariants(matchingSeriesIds)).products) || []
+
+  return isAccessoryOrSparePart ? (
+    <AccessoryOrSparePartPage product={product} matchingProducts={matchingProducts} />
+  ) : (
+    <ProductPageLayout>
+      <ProductTop product={product} />
+      <ProductMiddleTest product={product} />
+      {product.variants.length > 1 && <VariantTable product={product} />}
+    </ProductPageLayout>
+  )
+}
