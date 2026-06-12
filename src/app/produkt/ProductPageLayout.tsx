5608362cb5d8ecac6df1c5e6289dd3ccc6d323d1
Arshad A. Kamran <37771926+arshadkamran@users.noreply.github.com>
2026-06-12 11:59:52 +0200

    Knappen for sammenligning settes på produktsiden (#898)
    * knappen for sammenligning settes på produktsiden
    
    * justere styling

--- src/app/produkt/ProductPageLayout.tsx
+++ src/app/produkt/ProductPageLayout.tsx
@@ -1,17 +1,19 @@
 import { VStack } from '@navikt/ds-react'
-import { ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 
 export const ProductPageLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <VStack
-      gap={'space-56'}
-      paddingBlock={'space-64'}
-      paddingInline={'space-16'}
-      marginInline={'auto'}
-      marginBlock={'space-0'}
-      maxWidth={'1200px'}
-    >
-      {children}
-    </VStack>
+    <>
+      <VStack
+        gap={'space-56'}
+        paddingBlock={'space-64'}
+        paddingInline={'space-16'}
+        marginInline={'auto'}
+        marginBlock={'space-0'}
+        maxWidth={'1200px'}
+      >
+        {children}
+      </VStack>
+    </>
   )
 }
