96c7e1e1c5acadcf930e81347ad3d91148624084
Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
2026-06-24 14:14:48 +0200

    WIP: styling of news page
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- src/app/nyheter-test/NewsCard.tsx
+++ src/app/nyheter-test/NewsCard.tsx
@@ -1,4 +1,4 @@
-import { BodyLong, Heading, HStack, LinkCard, Tag, VStack } from '@navikt/ds-react'
+import { LinkCard, Tag } from '@navikt/ds-react'
 import { NewsDTO } from '@/app/nyheter-test/news-util'
 import Image from 'next/image'
 
@@ -8,11 +8,10 @@ type NewsProps = {
 
 export default function NewsCard({ news }: NewsProps){
 
-
   return (
-    <LinkCard key={news.id} style={{ height: '100%', minHeight: '180px' }}>
-      <LinkCard.Image aspectRatio="16/6">
-        <Image src="/assets/supreme_glorious_leader.png" alt="Alt-tekst for bilde" width="700" height="700" />
+    <LinkCard key={news.id} style={{ height: '100%', minHeight: '180px', width: '400px'}}>
+      <LinkCard.Image aspectRatio="16/9">
+        <Image fill src="/assets/supreme_glorious_leader.png" alt="Alt-tekst for bilde" />
       </LinkCard.Image>
       <LinkCard.Title>{news.title}</LinkCard.Title>
       <LinkCard.Description>{news.description}</LinkCard.Description>
--- src/app/nyheter-test/NewsGridPage.tsx
+++ src/app/nyheter-test/NewsGridPage.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { BodyLong, Heading, HGrid, HStack, LinkCard, VStack } from '@navikt/ds-react'
+import { BodyLong, Heading, HGrid, HStack, LinkCard, VStack, Page } from '@navikt/ds-react'
 import { NewsDTO } from '@/app/nyheter-test/news-util'
 import NewsCard from '@/app/nyheter-test/NewsCard'
 
@@ -10,19 +10,26 @@ type NewsProps = {
 
 export default function NewsGridPage({ news }: NewsProps) {
   return (
-    <VStack gap="space-8" margin="space-20">
-      <HStack justify="space-between" align="center">
-        <Heading size="large" level="1">
-          Nyheter
-        </Heading>
-      </HStack>
-
-      <HGrid gap="space-12" columns={{ xs: 'repeat(auto-fit, minmax(10rem, 1fr))', md: 3 }}>
-        {news?.map((news) => (
-          <NewsCard news={news} key={news.id} />
-        ))}
-        {news && news.length === 0 && <BodyLong>Ingen nyheter matchet søket ditt.</BodyLong>}
-      </HGrid>
-    </VStack>
+    <Page>
+      <Page.Block as="main" gutters>
+        <VStack justify={'center'}>
+          <Heading size="large" level="1">
+            Nyheter
+          </Heading>
+          <HStack justify="center">
+            <HGrid
+              position={'relative'}
+              gap={'space-20'}
+              columns={{ xs: 'repeat(auto-fit, minmax(10rem, 1fr))', md: 3 }}
+            >
+              {news?.map((news) => (
+                <NewsCard news={news} key={news.id} />
+              ))}
+              {news && news.length === 0 && <BodyLong>Ingen nyheter matchet søket ditt.</BodyLong>}
+            </HGrid>
+          </HStack>
+        </VStack>
+      </Page.Block>
+    </Page>
   )
 }
