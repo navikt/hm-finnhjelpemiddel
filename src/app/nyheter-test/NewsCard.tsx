cc2cfa1f7953dc437f5431a3d91233fd5375e7cc
Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
2026-06-24 10:10:37 +0200

    WIP: add default image for news
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- src/app/nyheter-test/NewsCard.tsx
+++ src/app/nyheter-test/NewsCard.tsx
@@ -1,5 +1,6 @@
 import { BodyLong, Heading, HStack, LinkCard, Tag, VStack } from '@navikt/ds-react'
 import { NewsDTO } from '@/app/nyheter-test/news-util'
+import Image from 'next/image'
 
 type NewsProps = {
   news: NewsDTO
@@ -11,12 +12,10 @@ export default function NewsCard({ news }: NewsProps){
   return (
     <LinkCard key={news.id} style={{ height: '100%', minHeight: '180px' }}>
       <LinkCard.Image aspectRatio="16/6">
-        <img src="/images/og/blogg/image-2.png" alt="Alt-tekst for bilde" width="700" />
+        <Image src="/assets/supreme_glorious_leader.png" alt="Alt-tekst for bilde" width="700" height="700" />
       </LinkCard.Image>
       <LinkCard.Title>{news.title}</LinkCard.Title>
-      <LinkCard.Description>
-        {news.description}
-      </LinkCard.Description>
+      <LinkCard.Description>{news.description}</LinkCard.Description>
       <LinkCard.Footer>
         <Tag size="small" variant="strong">
           Tag 1
