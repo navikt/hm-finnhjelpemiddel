1933832347901db0474af5a2c8572864e6de15bd
Tini.Tran <tini.tran@nav.no>
2026-06-24 15:13:31 +0200

    WIP: jobbet litt med nyhetssiden. Ønsker litt mer responsivness
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- src/app/nyheter-test/NewsCard.tsx
+++ src/app/nyheter-test/NewsCard.tsx
@@ -1,4 +1,4 @@
-import { LinkCard, Tag } from '@navikt/ds-react'
+import { LinkCard, Tag, Box } from '@navikt/ds-react'
 import { NewsDTO } from '@/app/nyheter-test/news-util'
 import Image from 'next/image'
 
@@ -9,18 +9,20 @@ type NewsProps = {
 export default function NewsCard({ news }: NewsProps){
 
   return (
-    <LinkCard key={news.id} style={{ height: '100%', minHeight: '180px', width: '400px'}}>
-      <LinkCard.Image aspectRatio="16/9">
-        <Image fill src="/assets/supreme_glorious_leader.png" alt="Alt-tekst for bilde" />
-      </LinkCard.Image>
-      <LinkCard.Title>{news.title}</LinkCard.Title>
-      <LinkCard.Description>{news.description}</LinkCard.Description>
-      <LinkCard.Footer>
-        <Tag size="small" variant="strong">
-          Tag 1
-        </Tag>
-        <Tag size="small">Tag 2</Tag>
-      </LinkCard.Footer>
-    </LinkCard>
+    <Box height={"27rem"}>
+      <LinkCard key={news.id} style={{ height: '100%', minHeight: '300px' }}>
+        <LinkCard.Image aspectRatio="16/9">
+          <Image fill src="/assets/supreme_glorious_leader.png" alt="Alt-tekst for bilde" />
+        </LinkCard.Image>
+        <LinkCard.Title>{news.title}</LinkCard.Title>
+        <LinkCard.Description>{news.description}</LinkCard.Description>
+        <LinkCard.Footer>
+          <Tag size="small" variant="strong">
+            Tag 1
+          </Tag>
+          <Tag size="small">Tag 2</Tag>
+        </LinkCard.Footer>
+      </LinkCard>
+    </Box>
   )
 }
--- src/app/nyheter-test/NewsGridPage.tsx
+++ src/app/nyheter-test/NewsGridPage.tsx
@@ -12,23 +12,22 @@ export default function NewsGridPage({ news }: NewsProps) {
   return (
     <Page>
       <Page.Block as="main" gutters>
-        <VStack justify={'center'}>
-          <Heading size="large" level="1">
-            Nyheter
-          </Heading>
-          <HStack justify="center">
-            <HGrid
-              position={'relative'}
-              gap={'space-20'}
-              columns={{ xs: 'repeat(auto-fit, minmax(10rem, 1fr))', md: 3 }}
-            >
-              {news?.map((news) => (
-                <NewsCard news={news} key={news.id} />
-              ))}
-              {news && news.length === 0 && <BodyLong>Ingen nyheter matchet søket ditt.</BodyLong>}
-            </HGrid>
-          </HStack>
-        </VStack>
+        <HStack justify={"center"} padding={'space-16'}>
+          <VStack gap={'space-8'}>
+            <Heading size="large" level="1">
+              Nyheter
+            </Heading>
+              <HGrid
+                gap={'space-20'}
+                columns={{ xs: 1,sm: 2, md: 3 }}
+              >
+                {news?.map((news) => (
+                  <NewsCard news={news} key={news.id} />
+                ))}
+                {news && news.length === 0 && <BodyLong>Ingen nyheter matchet søket ditt.</BodyLong>}
+              </HGrid>
+          </VStack>
+        </HStack>
       </Page.Block>
     </Page>
   )
