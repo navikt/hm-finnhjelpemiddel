244fed2d0abae6cb50d10caeda20b14b6cbd9c55
Tini.Tran <tini.tran@nav.no>
2026-06-25 13:33:54 +0200

    fix: byttet om NewsCard-komponentet slik at den ikke strekker seg når den får ingress.
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- src/app/nyheter-test/NewsCard.tsx
+++ src/app/nyheter-test/NewsCard.tsx
@@ -1,6 +1,9 @@
 import { LinkCard, Tag, Box } from '@navikt/ds-react'
 import { NewsDTO } from '@/app/nyheter-test/news-util'
 import Image from 'next/image'
+import { NewspaperIcon } from '@navikt/aksel-icons'
+import NextLink from 'next/link'
+import NewsImage from '@/app/nyheter-test/NewsImage'
 
 type NewsProps = {
   news: NewsDTO
@@ -9,20 +12,31 @@ type NewsProps = {
 export default function NewsCard({ news }: NewsProps){
 
   return (
-    <Box height={"27rem"}>
-      <LinkCard key={news.id} style={{ height: '100%', minHeight: '300px' }}>
+      <LinkCard key={news.id} style={{minHeight: "490px"}}>
         <LinkCard.Image aspectRatio="16/9">
-          <Image fill src="/assets/supreme_glorious_leader.png" alt="Alt-tekst for bilde" />
+          {/*<Image fill src="/assets/supreme_glorious_leader.png" alt="Alt-tekst for bilde"/> */}
+          <NewsImage fontSize={"5rem"}></NewsImage>
         </LinkCard.Image>
-        <LinkCard.Title>{news.title}</LinkCard.Title>
-        <LinkCard.Description>{news.description}</LinkCard.Description>
+        <LinkCard.Title>
+          <LinkCard.Anchor asChild>
+            <NextLink href={`/nyheter-test/${news.id}`}>
+              {news.title}
+            </NextLink>
+          </LinkCard.Anchor>
+        </LinkCard.Title>
+        <LinkCard.Description style={{
+          display: '-webkit-box',
+          WebkitLineClamp: 5,
+          WebkitBoxOrient: 'vertical',
+          overflow: 'hidden',
+        }}>{news.description}</LinkCard.Description>
         <LinkCard.Footer>
-          <Tag size="small" variant="strong">
-            Tag 1
+          <Box paddingBlock={'space-0 space-16'}>
+          <Tag size="small" variant="info-moderate">
+            Ny rammeavtale
           </Tag>
-          <Tag size="small">Tag 2</Tag>
+       </Box>
         </LinkCard.Footer>
       </LinkCard>
-    </Box>
   )
 }
--- src/app/nyheter-test/NewsGridPage.tsx
+++ src/app/nyheter-test/NewsGridPage.tsx
@@ -12,20 +12,17 @@ export default function NewsGridPage({ news }: NewsProps) {
   return (
     <Page>
       <Page.Block as="main" gutters>
-        <HStack justify={"center"} padding={'space-16'}>
-          <VStack gap={'space-8'}>
+        <HStack justify={'center'} padding={'space-16'}>
+          <VStack gap={'space-8'} style={{ width: '100%', maxWidth: '1200px' }}>
             <Heading size="large" level="1">
               Nyheter
             </Heading>
-              <HGrid
-                gap={'space-20'}
-                columns={{ xs: 1,sm: 2, md: 3 }}
-              >
-                {news?.map((news) => (
-                  <NewsCard news={news} key={news.id} />
-                ))}
-                {news && news.length === 0 && <BodyLong>Ingen nyheter matchet søket ditt.</BodyLong>}
-              </HGrid>
+            <HGrid gap={'space-20'} columns={{ xs: 1, sm: 2, md: 3 }}>
+              {news?.map((news) => (
+                <NewsCard news={news} key={news.id} />
+              ))}
+              {news && news.length === 0 && <BodyLong>Ingen nyheter matchet søket ditt.</BodyLong>}
+            </HGrid>
           </VStack>
         </HStack>
       </Page.Block>
