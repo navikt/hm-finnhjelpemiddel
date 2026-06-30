79a26e855fa33dfa1aacb3a6677898e998637736
Atle Nordland <atle.nordland@nav.no>
2026-06-30 13:38:19 +0200

    metadata for leverandører-siden

--- src/app/leverandorer/page.tsx
+++ src/app/leverandorer/page.tsx
@@ -1,78 +1,11 @@
-'use client'
+import { SupplierPage } from '@/app/leverandorer/SupplierPage'
+import { Metadata } from 'next'
 
-import SupplierList from '@/app/leverandorer/SupplierList'
-import { getAllSuppliers } from '@/utils/api-util'
-import { alphabet, Supplier } from '@/utils/supplier-util'
-import { BodyShort, Box, Heading, HStack, Link, Loader, VStack } from '@navikt/ds-react'
-import NextLink from 'next/link'
-import useSWR from 'swr'
-import styles from './SupplierPage.module.scss'
+export const metadata: Metadata = {
+  title: 'Leverandører',
+  description: 'Liste av leverandører',
+}
 
-const disabledLetters = ['Z', 'Æ', 'Å']
-
-export default function SearchPage() {
-  const { data: activeSuppliers, isLoading } = useSWR<Supplier[]>('/suppliers/_search', getAllSuppliers, {
-    keepPreviousData: true,
-    revalidateOnFocus: false,
-  })
-
-  return (
-    <div className={styles.suppliersPage}>
-      <Box
-        marginInline={'auto'}
-        marginBlock={"space-0"}
-        maxWidth={'1000px'}
-        paddingBlock={"space-0 space-48"}
-        paddingInline={{ xs: "space-16", md: "space-48" }}
-      >
-        <VStack
-          className={styles.pageContent}
-          gap={"space-16"}
-          paddingBlock={{ xs: "space-24", md: "space-48" }}
-          paddingInline={{ xs: "space-16", md: "space-48" }}
-        >
-          <div>
-            <Heading level="1" size="large" spacing>
-              Leverandører
-            </Heading>
-          </div>
-          <BodyShort spacing>
-            Nedenfor finner du en liste av leverandører med produkter på finnhjelpemiddel.no
-          </BodyShort>
-
-          {!isLoading && activeSuppliers && (
-            <>
-              <HStack gap="space-8" paddingBlock={{ xs: "space-0 space-32", md: "space-0 space-48" }}>
-                {alphabet.map((letter) => (
-                  <span
-                    className={disabledLetters.includes(letter) ? styles.letterDisabled : styles.letter}
-                    aria-hidden={disabledLetters.includes(letter) ? 'true' : 'false'}
-                    key={letter}
-                  >
-                    <Link as={NextLink} href={`#${letter}`} key={letter}>
-                      {letter}
-                    </Link>
-                  </span>
-                ))}
-              </HStack>
-
-              {alphabet.map((letter) => (
-                <SupplierList
-                  key={letter}
-                  letter={letter}
-                  suppliers={activeSuppliers.filter((value) => value.name.at(0)?.toUpperCase() === letter)}
-                />
-              ))}
-            </>
-          )}
-
-          {isLoading && (
-            <HStack justify="center" style={{ marginTop: '18px' }}>
-              <Loader size="xlarge" title="Laster leverandører" />
-            </HStack>
-          )}
-        </VStack>
-      </Box>
-    </div>
-  );
+export default function Page() {
+  return <SupplierPage />
 }
