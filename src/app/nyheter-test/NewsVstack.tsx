58218665bdbb481efb712412a7eecd43cfa0648a
Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>
2026-07-01 13:28:17 +0200

    style: make title in newsfeed align with title in agreement
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>

--- src/app/nyheter-test/NewsVstack.tsx
+++ src/app/nyheter-test/NewsVstack.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 export default function NewsVstack({ news }: Props) {
   return (
-    <VStack gap="space-16"  maxWidth={'600px'} width={'100%'} paddingBlock="space-24">
+    <VStack gap="space-16"  maxWidth={'600px'} width={'100%'}>
       <Heading level={'2'} size={'large'}>Aktuelt</Heading>
       {news?.map((news) => (
         <SmallNewsCard news={news} key={news.id} />
