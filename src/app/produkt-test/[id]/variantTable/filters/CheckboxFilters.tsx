1b5437bbdf6744f37112dc974d3af4bc9046b8ae
Erlend.Andre.Hontorp <erlend.andre.hontorp@nav.no>
2026-07-02 09:05:11 +0200

    wip: "På avtale med Nav" chip to checkbox (still missing checkbox when finding product via hms number)
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Andreas Danielsen Fageraas <andreas.danielsen.fageraas@nav.no>
    Co-authored-by: Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>

---
+++ src/app/produkt-test/[id]/variantTable/filters/CheckboxFilters.tsx
@@ -0,0 +1,30 @@
+import { useSearchParams } from 'next/navigation'
+import { Checkbox } from '@navikt/ds-react'
+import { FilterContent } from '@/app/produkt-test/[id]/variantTable/filters/FilterRowTest'
+
+// TODO: se kommentar fra Atle for resterende fix
+
+export const AgreementCheckbox = ({
+  filters,
+  onFilterChange,
+}: {
+  filters: FilterContent[]
+  onFilterChange: (name: string, value: string) => void
+}) => {
+  const searchParams = useSearchParams()
+
+  return (
+    <>
+      {filters.map(({ name, label, values }) => (
+        <Checkbox
+          key={name}
+          checked={searchParams.has(name)}
+          onChange={(event) => onFilterChange(name, event.target.checked ? 'true' : '')}
+          disabled={values.length < 2}
+        >
+          {label}
+        </Checkbox>
+      ))}
+    </>
+  )
+}
