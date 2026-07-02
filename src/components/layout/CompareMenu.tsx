a256438167a65fe9b1fca304d551c7f1f7771b98
Andreas.Danielsen.Fageraas <andreas.danielsen.fageraas@nav.no>
2026-07-02 13:19:08 +0200

    fix: made comparison menu show as long as there is one or more products selected
    Co-authored-by: Tini Tran <tini.tran@nav.no>
    Co-authored-by: Erlend André Høntorp <erlend.andre.hontorp@nav.no>
    Co-authored-by: Maria Agnetha Andreassen <maria.agnetha.andreassen@nav.no>

--- src/components/layout/CompareMenu.tsx
+++ src/components/layout/CompareMenu.tsx
@@ -1,11 +1,12 @@
 import { ChevronDownIcon, ChevronRightIcon, ChevronUpIcon, TrashIcon } from '@navikt/aksel-icons'
-import { Button } from '@navikt/ds-react'
+import { BodyShort, Button } from '@navikt/ds-react'
 
 import { CompareMenuState, useHydratedCompareStore } from '@/utils/global-state-util'
 
 import classNames from 'classnames'
 import ProductCardCompare from '@/components/ProductCardCompare'
 import NextLink from 'next/link'
+import { useEffect } from 'react'
 
 const CompareMenu = () => {
   const { compareMenuState, productsToCompare, setCompareMenuState, resetProductToCompare } = useHydratedCompareStore()
@@ -14,7 +15,13 @@ const CompareMenu = () => {
 
   const reversedProductsToCompare = productsToCompare.slice().reverse()
 
-  if (productsToCompare.length < 2) {
+  useEffect(() => {
+    if (productsToCompare.length === 2) {
+      setCompareMenuState(CompareMenuState.Open)
+    }
+  }, [productsToCompare.length])
+
+  if (productsToCompare.length === 0) {
     return <></>
   }
 
@@ -58,23 +65,29 @@ const CompareMenu = () => {
               </li>
             ))}
           </ul>
-          <div className="compare-menu__buttons">
-            <NextLink href={compareUrl} passHref>
-              <Button as="a" icon={<ChevronRightIcon aria-hidden />} iconPosition="right">
-                Sammenlign
+          {productsToCompare.length >= 2 ? (
+            <div className="compare-menu__buttons">
+              <NextLink href={compareUrl} passHref>
+                <Button as="a" icon={<ChevronRightIcon aria-hidden />} iconPosition="right">
+                  Sammenlign
+                </Button>
+              </NextLink>
+              <Button
+                variant="tertiary"
+                icon={<TrashIcon aria-hidden />}
+                onClick={() => {
+                  resetProductToCompare()
+                  setCompareMenuState(CompareMenuState.Minimized)
+                }}
+              >
+                Nullstill
               </Button>
-            </NextLink>
-            <Button
-              variant="tertiary"
-              icon={<TrashIcon aria-hidden />}
-              onClick={() => {
-                resetProductToCompare()
-                setCompareMenuState(CompareMenuState.Minimized)
-              }}
-            >
-              Nullstill
-            </Button>
-          </div>
+            </div>
+          ) : (
+            <div className="compare-menu__placeholder compare-menu__placeholder__one-more">
+              <BodyShort>Velg minst ett produkt til for å gå til sammenligning.</BodyShort>
+            </div>
+          )}
         </div>
       )}
     </div>
