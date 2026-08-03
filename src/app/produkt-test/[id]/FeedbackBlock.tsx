b5d7ddfa3dbde9a27b9e7abddf133cf60f1b978d
Atle Nordland <atle.nordland@nav.no>
2026-08-03 14:52:44 +0200

    ny visning-tekst

--- src/app/produkt-test/[id]/FeedbackBlock.tsx
+++ src/app/produkt-test/[id]/FeedbackBlock.tsx
@@ -25,56 +25,58 @@ export const FeedbackBlock = ({ setBetaEnabled }: FeedbackBlockProps) => {
   })
 
   return (
-    <Box
-      as={VStack}
-      gap={'space-24'}
-      paddingBlock={'space-16'}
-      paddingInline={'space-32'}
-      background={'warning-soft'}
-      width={{ xs: '100%', sm: '500px' }}
-      borderRadius={'8'}
-      borderColor={'warning-subtle'}
-      borderWidth={'1'}
-      style={{ alignSelf: 'center' }}
-    >
-      <VStack gap={'space-16'} justify={'space-between'}>
-        {!expandFeedback && (
-          <>
-            Dette er den nye visningen.
-            <BodyShort>
-              Vi har gjort siden mer oversiktlig, for å enklere finne riktig variant.
-              {consent && ' Gi oss gjerne tilbakemeldinger på endringene.'}
-            </BodyShort>
-          </>
-        )}
-        <HStack gap={'space-16'} justify={'space-between'} width={'100%'}>
-          {!expandFeedback && consent === 'true' && (
-            <Button size={'small'} onClick={() => setExpandFeedback((value) => !value)}>
-              Gi tilbakemelding
-            </Button>
+    <Box paddingInline={'space-8'} style={{ alignSelf: 'center', marginTop: '-40px' }}>
+      <Box
+        as={VStack}
+        gap={'space-24'}
+        paddingBlock={'space-16'}
+        paddingInline={{ xs: 'space-16', md: 'space-32' }}
+        background={'warning-soft'}
+        width={{ xs: '100%', sm: '500px' }}
+        borderRadius={'8'}
+        borderColor={'warning-subtle'}
+        borderWidth={'1'}
+      >
+        <VStack gap={'space-16'} justify={'space-between'}>
+          {!expandFeedback && (
+            <>
+              Dette er den nye visningen.
+              <BodyShort>
+                Vi har gjort siden mer oversiktlig, for å enklere finne riktig variant.
+                {consent && ' Gi oss gjerne tilbakemeldinger på endringene.'}
+              </BodyShort>
+            </>
           )}
-          <Link
-            onClick={() => {
-              if (typeof window !== 'undefined') {
-                logUmamiClickButton('Tilbake til gammel versjon', 'new-product-page-toggle', 'action')
-                localStorage.setItem(localStorageProductPageBeta, 'false')
-                setBetaEnabled('false')
-              }
-            }}
-            style={{ marginLeft: 'auto' }}
-          >
-            Tilbake til gammel versjon
-          </Link>
-        </HStack>
-      </VStack>
-      {expandFeedback && (
-        <Box>
-          <div>
-            {/* @ts-expect-error Ikke typet */}
-            <skyra-survey slug="arbeids-og-velferdsetaten-nav/finnhjelpemiddel-ny-produktside"></skyra-survey>
-          </div>
-        </Box>
-      )}
+
+          <HStack gap={'space-16'} justify={'space-between'} width={'100%'}>
+            {!expandFeedback && consent === 'true' && (
+              <Button size={'small'} onClick={() => setExpandFeedback((value) => !value)}>
+                Gi tilbakemelding
+              </Button>
+            )}
+            <Link
+              onClick={() => {
+                if (typeof window !== 'undefined') {
+                  logUmamiClickButton('Tilbake til gammel versjon', 'new-product-page-toggle', 'action')
+                  localStorage.setItem(localStorageProductPageBeta, 'false')
+                  setBetaEnabled('false')
+                }
+              }}
+              style={{ marginLeft: 'auto' }}
+            >
+              Tilbake til gammel versjon
+            </Link>
+          </HStack>
+        </VStack>
+        {expandFeedback && (
+          <Box>
+            <div>
+              {/* @ts-expect-error Ikke typet */}
+              <skyra-survey slug="arbeids-og-velferdsetaten-nav/finnhjelpemiddel-ny-produktside"></skyra-survey>
+            </div>
+          </Box>
+        )}
+      </Box>
     </Box>
   )
 }
