61feb9bff7249ad8a20663be9ae3c9a194b29124
marte pedersen <marte.pedersen@nav.no>
2026-08-19 16:01:32 +0200

    serverside redirect på hmsnr-url

--- src/app/produkt-test/hmsartnr/[hmsartnr]/page.tsx
+++ src/app/produkt-test/hmsartnr/[hmsartnr]/page.tsx
@@ -1,37 +1,15 @@
-import { ProductTestPage } from '@/app/produkt-test/[id]/ProductTestPage'
-import AccessoryOrSparePartPage from '@/app/produkt/AccessoryOrSparePartPage'
+import { redirect } from 'next/navigation'
 
-import { Metadata } from 'next'
-
-import { fetchProductsWithVariants, getProductByHmsartnrWithVariants } from '@/utils/api-util'
+import { getProductByHmsartnrWithVariants } from '@/utils/api-util'
 import { mapProductFromHmsArtNr } from '@/utils/product-util'
 
 type Props = {
   params: Promise<{ hmsartnr: string }>
 }
 
-export async function generateMetadata(props: Props): Promise<Metadata> {
-  const params = await props.params
-  const product = mapProductFromHmsArtNr(await getProductByHmsartnrWithVariants(params.hmsartnr), params.hmsartnr)
-
-  return {
-    title: product.title,
-    description: 'Produktside for ' + product.title,
-  }
-}
-
 export default async function ProduktPage(props: Props) {
   const params = await props.params
 
   const product = mapProductFromHmsArtNr(await getProductByHmsartnrWithVariants(params.hmsartnr), params.hmsartnr)
-  const isAccessoryOrSparePart = !product.main
-  const matchingSeriesIds = product.attributes.compatibleWith?.seriesIds
-
-  const matchingProducts = (matchingSeriesIds && (await fetchProductsWithVariants(matchingSeriesIds)).products) || []
-
-  return isAccessoryOrSparePart ? (
-    <AccessoryOrSparePartPage product={product} matchingProducts={matchingProducts} />
-  ) : (
-    <ProductTestPage product={product} hmsartnr={params.hmsartnr} />
-  )
+  redirect(`/produkt-test/${product.id}?term=${params.hmsartnr}`)
 }
