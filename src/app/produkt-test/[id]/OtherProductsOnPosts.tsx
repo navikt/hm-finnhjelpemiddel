91b7f06746d648945a626e1e70cac32fc6c9e711
Atle Nordland <atle.nordland@nav.no>
2026-09-15 14:55:38 +0200

    wip

--- src/app/produkt-test/[id]/OtherProductsOnPosts.tsx
+++ src/app/produkt-test/[id]/OtherProductsOnPosts.tsx
@@ -50,7 +50,7 @@ const OtherProductsOnPost = ({ agreement, seriesId }: { agreement: AgreementInfo
         {agreement.postTitle}
       </Link>
 
-      <HStack gap={'space-24'} paddingBlock={'space-0 space-8'}>
+      <HStack gap={'space-24'} paddingBlock={'space-0 space-8'} align={'center'}>
         {agreement.postTitle &&
           data.products
             .sort((a, b) => {
